ddfec4e71385a0748bb25a59f4c3b0df5f190770
S.Cao <scao.math@gmail.com>
2021-04-18 13:16:17 -0500

    Fixed the dt in the burgers data gen script

--- data_generation/burgers/burgers1.m
+++ data_generation/burgers/burgers1.m
@@ -1,8 +1,9 @@
 function u = burgers1(init, tspan, s, visc)
 
 S = spinop([0 1], tspan);
+dt = tspan(2) - tspan(1);
 S.lin = @(u) + visc*diff(u,2);
 S.nonlin = @(u) - 0.5*diff(u.^2);
 S.init = init;
-u = spin(S,s,1e-4,'plot','off'); 
+u = spin(S,s,dt,'plot','off'); 
 
