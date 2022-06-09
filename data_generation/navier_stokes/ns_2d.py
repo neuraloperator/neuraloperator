6bedfe747127184b97fc20a4d93233db428d8237
Nis Meinert <nis.meinert@simulation.science>
2022-06-09 09:54:03 +0200

    Replace FFT with rFFT

--- data_generation/navier_stokes/ns_2d.py
+++ data_generation/navier_stokes/ns_2d.py
@@ -31,10 +31,10 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
     steps = math.ceil(T/delta_t)
 
     #Initial vorticity to Fourier space
-    w_h = torch.fft.fft2(w0)
+    w_h = torch.fft.rfft2(w0)
 
     #Forcing to Fourier space
-    f_h = torch.fft.fft2(f)
+    f_h = torch.fft.rfft2(f)
 
     #If same forcing for the whole batch
     if len(f_h.size()) < len(w_h.size()):
@@ -47,6 +47,11 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
     k_y = torch.cat((torch.arange(start=0, end=k_max, step=1, device=w0.device), torch.arange(start=-k_max, end=0, step=1, device=w0.device)), 0).repeat(N,1)
     #Wavenumbers in x-direction
     k_x = k_y.transpose(0,1)
+
+    #Truncate redundant modes
+    k_x = k_x[..., :k_max + 1]
+    k_y = k_y[..., :k_max + 1]
+
     #Negative Laplacian in Fourier space
     lap = 4*(math.pi**2)*(k_x**2 + k_y**2)
     lap[0,0] = 1.0
@@ -67,22 +72,22 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
 
         #Velocity field in x-direction = psi_y
         q = 2. * math.pi * k_y * 1j * psi_h
-        q = torch.fft.ifft2(q, s=(N, N)).real
+        q = torch.fft.irfft2(q, s=(N, N))
 
         #Velocity field in y-direction = -psi_x
         v = -2. * math.pi * k_x * 1j * psi_h
-        v = torch.fft.ifft2(v, s=(N, N)).real
+        v = torch.fft.irfft2(v, s=(N, N))
 
         #Partial x of vorticity
         w_x = 2. * math.pi * k_x * 1j * w_h
-        w_x = torch.fft.ifft2(w_x, s=(N, N)).real
+        w_x = torch.fft.irfft2(w_x, s=(N, N))
 
         #Partial y of vorticity
         w_y = 2. * math.pi * k_y * 1j * w_h
-        w_y = torch.fft.ifft2(w_y, s=(N, N)).real
+        w_y = torch.fft.irfft2(w_y, s=(N, N))
 
         #Non-linear term (u.grad(w)): compute in physical space then back to Fourier space
-        F_h = torch.fft.fft2(q*w_x + v*w_y)
+        F_h = torch.fft.rfft2(q*w_x + v*w_y)
 
         #Dealias
         F_h = dealias* F_h
@@ -95,10 +100,10 @@ def navier_stokes_2d(w0, f, visc, T, delta_t=1e-4, record_steps=1):
 
         if (j+1) % record_time == 0:
             #Solution in physical space
-            w = torch.fft.ifft2(w_h, s=(N, N))
+            w = torch.fft.irfft2(w_h, s=(N, N))
 
             #Record solution and time
-            sol[...,c] = w.real
+            sol[...,c] = w
             sol_t[c] = t
 
             c += 1
