15a7d8084ee3f4f3210141df929a2d7116148b06
liu-ziyuan-math <70881298+liu-ziyuan-math@users.noreply.github.com>
2022-06-21 16:27:10 +0800

    Update lowrank_2d.py

--- lowrank_2d.py
+++ lowrank_2d.py
@@ -18,7 +18,9 @@ from utilities import *
 
 torch.manual_seed(0)
 np.random.seed(0)
-
+if device == torch.device('cuda'):
+    torch.cuda.manual_seed(0)
+    torch.backends.cudnn.deterministic = True
 
 
 class LowRank2d(nn.Module):
