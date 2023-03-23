a72ba67578025b43d9171acedf3e337a2ff9c0d2
Ubuntu <ubuntu@ip-172-31-1-6.us-west-2.compute.internal>
2023-03-23 19:35:23 +0000

    module done

---
+++ neuralop/models/tests/test_uno.py
@@ -0,0 +1,22 @@
+import time
+from torchsummary import summary
+from neuralop.models.tfno import UNO
+import torch
+layer_configs = [{"out_channels":20, "n_modes" : [5,5], "res_scaling" :[0.5,0.5] },\
+                {"out_channels":20, "n_modes" : [5,5], "res_scaling" :[1,1] },\
+                {"out_channels":20, "n_modes" : [5,5], "res_scaling" :[1,1] },\
+                {"out_channels":20, "n_modes" : [5,5], "res_scaling" :[2,2] },\
+                {"out_channels":10, "n_modes" : [5,5], "res_scaling" :[2,2] },\
+                        ]
+horizontal_skips_map ={4:0,3:1}
+model = UNO(3,3,5,layer_configs = layer_configs, horizontal_skips_map = horizontal_skips_map, n_layers = 5, domain_padding = 0.2, output_scale_factor = 2)
+summary(model,(3,20,20))
+
+t1 = time.time()
+in_data = torch.randn(10,3,20,20).to('cuda')
+out = model(in_data)
+t = time.time() - t1
+print(f'Output of size {out.shape} in {t}.')
+
+loss = out.sum()
+loss.backward()
