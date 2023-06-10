fixed the padding function, more test cases for UNO test, calculate end-to-end scaling factor from layerwise scaling factors

neuralop/models/padding.py
@@ -54,36 +54,31 @@ class DomainPadding(nn.Module):
             padding = [int(round(p*r)) for (p, r) in zip(self.domain_padding, resolution)]
             
             print(f'Padding inputs of {resolution=} with {padding=}, {self.padding_mode}')
-
+            
+            
             output_pad = padding
-
-#             for scale_factor in self.output_scale_factor:
-#                 if isinstance(scale_factor, (float, int)):
-#                     scale_factor = [scale_factor]*len(resolution)
             output_pad = [int(round(i*j)) for (i,j) in zip(self.output_scale_factor,output_pad)]
 
             if self.padding_mode == 'symmetric':
                 # Pad both sides
-                unpad_indices = (Ellipsis, ) + tuple([slice(p, -p, None) for p in output_pad ])
+                unpad_indices = (Ellipsis, ) + tuple([slice(p, -p, None) for p in output_pad[::-1] ])
                 padding = [i for p in padding for i in (p, p)]
 
             elif self.padding_mode == 'one-sided':
                 # One-side padding
-                unpad_indices = (Ellipsis, ) + tuple([slice(None, -p, None) for p in output_pad])
+                unpad_indices = (Ellipsis, ) + tuple([slice(None, -p, None) for p in output_pad[::-1]])
                 padding = [i for p in padding for i in (0, p)]
             else:
                 raise ValueError(f'Got {self.padding_mode=}')
             
             self._padding[f'{resolution}'] = padding
+            
+
 
             padded = F.pad(x, padding, mode='constant')
 
             out_put_shape = padded.shape[2:]
-#             for scale_factor in self.output_scale_factor:
-#                 if isinstance(scale_factor, (float, int)):
-#                     scale_factor = [scale_factor]*len(resolution)
             out_put_shape = [int(round(i*j)) for (i,j) in zip(self.output_scale_factor,out_put_shape)]
-            
             self._unpad_indices[f'{[i for i in out_put_shape]}'] = unpad_indices
 
             return padded
@@ -93,3 +88,4 @@ class DomainPadding(nn.Module):
         """
         unpad_indices = self._unpad_indices[f'{list(x.shape[2:])}']
         return x[unpad_indices]
+    

neuralop/models/spectral_convolution.py
@@ -480,8 +480,8 @@ class SpectralConvKernel2d(FactorizedSpectralConv):
         
         hm1, hm2 = self.half_n_modes[0], self.half_n_modes[1]
         if self.factorization is None:
-            self.W1 = nn.Parameter(torch.empty(self.in_channels, hm1, hm2, hm1, hm2, dtype = torch.cfloat))
-            self.W2 = nn.Parameter(torch.empty(self.in_channels, hm1, hm2, hm1, hm2, dtype = torch.cfloat))
+            self.W1 = nn.Parameter(torch.empty(hm1, hm2, hm1, hm2, dtype = torch.cfloat))
+            self.W2 = nn.Parameter(torch.empty(hm1, hm2, hm1, hm2, dtype = torch.cfloat))
             self.reset_parameter()
         self.sht_grid = sht_grid
         self.sht_norm = sht_norm
@@ -509,7 +509,7 @@ class SpectralConvKernel2d(FactorizedSpectralConv):
         torch.nn.init.normal_(self.W2, mean=0.0, std=scaling_factor)
     
     def mode_mixer(self, input, weights):
-        return torch.einsum("bimn,imnop->biop", input, weights)
+        return torch.einsum("bimn,mnop->biop", input, weights)
     
     def forward(self, x, indices=0, output_shape = None):
         batchsize, channels, height, width = x.shape

neuralop/models/uno.py
@@ -12,6 +12,7 @@ from .fno_block import FNOBlocks,resample
 from .tfno import partialclass
 from .attention import TnoBlock2d
 from .spectral_convolution import SpectralConvKernel2d
+import numpy as np
 #this will be merged with the neural operator UNO
 
 class UNO(nn.Module):
@@ -120,7 +121,6 @@ class UNO(nn.Module):
                  domain_padding=None,
                  domain_padding_mode='one-sided',
                  fft_norm='forward',
-                 output_scaling_factor = 1,
                  normalizer = None,  #only have efect on the transformer block. Nevertheless it might be a bad idea to include normalization as part of layer
                  **kwargs):
         super().__init__()
@@ -153,7 +153,6 @@ class UNO(nn.Module):
         self.separable = separable
         self.preactivation = preactivation
         self._incremental_n_modes = incremental_n_modes
-        self.output_scaling_factor = output_scaling_factor
         self.operator_block = operator_block
         self.integral_operator = integral_operator
         
@@ -161,11 +160,18 @@ class UNO(nn.Module):
             self.horizontal_skips_map = {}
             for i in range(n_layers//2,0,):
                 self.horizontal_skips_map[n_layers - i -1] = i
-                
+
+        self.output_scaling_factor = np.ones_like(self.uno_scalings[0])
+        for k in self.uno_scalings:
+            self.output_scaling_factor = np.multiply(self.output_scaling_factor, k)
+        self.output_scaling_factor = self.output_scaling_factor.tolist()
+        if len(self.output_scaling_factor) == 1:
+            self.output_scaling_factor = self.output_scaling_factor[0]
+
         if isinstance(self.output_scaling_factor, (float, int)):
             self.output_scaling_factor = [self.output_scaling_factor]*self.n_dim
         
-
+        print("calculated out factor", self.output_scaling_factor)
         if domain_padding is not None and domain_padding > 0:
             self.domain_padding = DomainPadding(domain_padding=domain_padding, padding_mode=domain_padding_mode\
             , output_scale_factor = self.output_scaling_factor)
@@ -186,7 +192,7 @@ class UNO(nn.Module):
 
             if i in self.horizontal_skips_map.keys():
                 prev_out = prev_out + self.uno_out_channels[self.horizontal_skips_map[i]]
-            print([self.uno_scalings[i]])
+            print(self.uno_scalings[i])
 
             self.fno_blocks.append(self.operator_block(
                                             in_channels=prev_out,
@@ -195,7 +201,7 @@ class UNO(nn.Module):
                                             use_mlp=use_mlp, 
                                             mlp_dropout=mlp_dropout, 
                                             mlp_expansion=mlp_expansion,
-                                            output_scaling_factor = self.uno_scalings[i],
+                                            output_scaling_factor = [self.uno_scalings[i]],
                                             non_linearity=non_linearity,
                                             norm=norm, preactivation=preactivation,
                                             fno_skip=fno_skip,
@@ -225,6 +231,7 @@ class UNO(nn.Module):
 
         if self.domain_padding is not None:
             x = self.domain_padding.pad(x)
+        print(x.shape)
         output_shape = [int(round(i*j)) for (i,j) in zip(x.shape[-self.n_dim:], self.output_scaling_factor)]
         
         skip_outputs = {}
@@ -241,6 +248,8 @@ class UNO(nn.Module):
             if layer_idx == self.n_layers -1:
                 cur_output = output_shape
             x = self.fno_blocks[layer_idx](x, output_shape = cur_output)
+            
+            print(x.shape)
 
             if layer_idx in self.horizontal_skips_map.values():
                 #print("saving skip", layer_idx)