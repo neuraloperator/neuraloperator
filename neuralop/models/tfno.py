337d0d22f343bce7ea1080092e4c37251f835502
“btolooshams” <btolooshams@gmail.com>
2023-07-21 15:18:45 -0700

    using outchannel in lifting as hidden channel when n_layers=1

--- neuralop/models/tfno.py
+++ neuralop/models/tfno.py
@@ -169,8 +169,7 @@ class FNO(nn.Module):
             SpectralConv=SpectralConv,
             n_layers=n_layers)
 
-        self.lifting = MLP(in_channels=in_channels, out_channels=self.hidden_channels, hidden_channels=self.lifting_channels, 
-                 n_layers=1, n_dim=self.n_dim)
+        self.lifting = MLP(in_channels=in_channels, out_channels=self.hidden_channels, hidden_channels=self.hidden_channels, n_layers=1, n_dim=self.n_dim)
         self.projection = MLP(in_channels=self.hidden_channels, out_channels=out_channels, hidden_channels=self.projection_channels, n_layers=2, n_dim=self.n_dim, non_linearity=non_linearity) 
 
     def forward(self, x):
