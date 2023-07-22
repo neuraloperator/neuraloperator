e3a2a52085e62d208623b40039100cdd553fd2af
Jean Kossaifi <jean.kossaifi@gmail.com>
2023-07-21 20:39:46 -0700

    SpectralConv docstrings

--- neuralop/layers/spectral_convolution.py
+++ neuralop/layers/spectral_convolution.py
@@ -417,6 +417,11 @@ class SubConv(nn.Module):
 
 
 class SpectralConv1d(SpectralConv):
+    """1D Spectral Conv
+    
+    This is provided for reference only, 
+    see :class:`neuralop.layers.SpectraConv` for the preferred, general implementation
+    """
     def forward(self, x, indices=0):
         batchsize, channels, width = x.shape
 
@@ -437,6 +442,11 @@ class SpectralConv1d(SpectralConv):
 
 
 class SpectralConv2d(SpectralConv):
+    """2D Spectral Conv, see :class:`neuralop.layers.SpectraConv` for the general case
+        
+    This is provided for reference only, 
+    see :class:`neuralop.layers.SpectraConv` for the preferred, general implementation
+    """
     def forward(self, x, indices=0):
         batchsize, channels, height, width = x.shape
 
@@ -465,6 +475,11 @@ class SpectralConv2d(SpectralConv):
 
 
 class SpectralConv3d(SpectralConv):
+    """3D Spectral Conv, see :class:`neuralop.layers.SpectraConv` for the general case
+    
+    This is provided for reference only, 
+    see :class:`neuralop.layers.SpectraConv` for the preferred, general implementation
+    """
     def forward(self, x, indices=0):
         batchsize, channels, height, width, depth = x.shape
 
