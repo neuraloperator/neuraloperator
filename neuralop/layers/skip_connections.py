9025b9a17797c8bcb3840c9ee9b272b6960d5831
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2023-07-23 14:26:32 -0700

    `autopep8 models/`

--- neuralop/layers/skip_connections.py
+++ neuralop/layers/skip_connections.py
@@ -2,7 +2,13 @@ import torch
 from torch import nn
 
 
-def skip_connection(in_features, out_features, n_dim=2, bias=False, type="soft-gating"):
+def skip_connection(
+        in_features,
+        out_features,
+        n_dim=2,
+        bias=False,
+        type="soft-gating"
+):
     """A wrapper for several types of skip connections.
     Returns an nn.Module skip connections, one of  {'identity', 'linear', soft-gating'}
 
@@ -14,7 +20,7 @@ def skip_connection(in_features, out_features, n_dim=2, bias=False, type="soft-g
         number of output features
     n_dim : int, default is 2
         Dimensionality of the input (excluding batch-size and channels).
-        ``n_dim=2`` corresponds to having Module2D. 
+        ``n_dim=2`` corresponds to having Module2D.
     bias : bool, optional
         whether to use a bias, by default False
     type : {'identity', 'linear', soft-gating'}
@@ -26,13 +32,26 @@ def skip_connection(in_features, out_features, n_dim=2, bias=False, type="soft-g
         module that takes in x and returns skip(x)
     """
     if type.lower() == 'soft-gating':
-        return SoftGating(in_features=in_features, out_features=out_features, bias=bias, n_dim=n_dim)
+        return SoftGating(
+            in_features=in_features,
+            out_features=out_features,
+            bias=bias,
+            n_dim=n_dim
+        )
     elif type.lower() == 'linear':
-        return getattr(nn, f'Conv{n_dim}d')(in_channels=in_features, out_channels=out_features, kernel_size=1, bias=bias)
+        return getattr(
+            nn,
+            f'Conv{n_dim}d')(
+            in_channels=in_features,
+            out_channels=out_features,
+            kernel_size=1,
+            bias=bias
+        )
     elif type.lower() == 'identity':
         return nn.Identity()
     else:
-        raise ValueError(f"Got skip-connection {type=}, expected one of {'soft-gating', 'linear', 'id'}.")
+        raise ValueError(
+            f"Got skip-connection {type=}, expected one of {'soft-gating', 'linear', 'id'}.")
 
 
 class SoftGating(nn.Module):
@@ -49,9 +68,10 @@ class SoftGating(nn.Module):
         this is provided for API compatibility with nn.Linear only
     n_dim : int, default is 2
         Dimensionality of the input (excluding batch-size and channels).
-        ``n_dim=2`` corresponds to having Module2D. 
+        ``n_dim=2`` corresponds to having Module2D.
     bias : bool, default is False
     """
+
     def __init__(self, in_features, out_features=None, n_dim=2, bias=False):
         super().__init__()
         if out_features is not None and in_features != out_features:
@@ -59,9 +79,13 @@ class SoftGating(nn.Module):
                              "but these two must be the same for soft-gating")
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = nn.Parameter(torch.ones(1, self.in_features, *(1,)*n_dim))
+        self.weight = nn.Parameter(torch.ones(
+            1, self.in_features, *(1,) * n_dim
+        ))
         if bias:
-            self.bias = nn.Parameter(torch.ones(1, self.in_features, *(1,)*n_dim))
+            self.bias = nn.Parameter(torch.ones(
+                1, self.in_features, *(1,) * n_dim
+            ))
         else:
             self.bias = None
 
@@ -69,7 +93,6 @@ class SoftGating(nn.Module):
         """Applies soft-gating to a batch of activations
         """
         if self.bias is not None:
-            return self.weight*x + self.bias
+            return self.weight * x + self.bias
         else:
-            return self.weight*x
-
+            return self.weight * x
