14ed438114f358986253e705902924479a943dd8
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2023-08-21 12:37:19 -0700

    `black integral_transform.py model_dispatcher.py`

--- neuralop/models/model_dispatcher.py
+++ neuralop/models/model_dispatcher.py
@@ -1,25 +1,25 @@
+import inspect
+
 from .fno import TFNO, TFNO1d, TFNO2d, TFNO3d
 from .fno import FNO, FNO1d, FNO2d, FNO3d
 from .uno import UNO
-import inspect
 
 
 MODEL_ZOO = {
-    'tfno'   : TFNO,
-    'tfno1d' : TFNO1d,
-    'tfno2d' : TFNO2d,
-    'tfno3d' : TFNO3d,
-    'fno'    : FNO,
-    'fno1d'  : FNO1d,
-    'fno2d'  : FNO2d,
-    'fno3d'  : FNO3d,
-    'uno'    : UNO,
+    "tfno": TFNO,
+    "tfno1d": TFNO1d,
+    "tfno2d": TFNO2d,
+    "tfno3d": TFNO3d,
+    "fno": FNO,
+    "fno1d": FNO1d,
+    "fno2d": FNO2d,
+    "fno3d": FNO3d,
+    "uno": UNO,
 }
 
 
 def available_models():
-    """List the available neural operators
-    """
+    """List the available neural operators"""
     return list(MODEL_ZOO.keys())
 
 
@@ -33,7 +33,7 @@ def get_model(config):
     Parameters
     ----------
     config : Bunch or dict-like
-        configuration, must have 
+        configuration, must have
         arch = config['arch'] (string)
         and the corresponding config[arch] (a subdict with the kwargs of the model)
 
@@ -42,30 +42,30 @@ def get_model(config):
     model : nn.Module
         the instanciated module
     """
-    arch = config['arch'].lower()
+    arch = config["arch"].lower()
     config_arch = config.get(arch)
 
     # Set the number of input channels depending on channels in data + mg patching
-    data_channels = config_arch.pop('data_channels')
+    data_channels = config_arch.pop("data_channels")
     try:
-        patching_levels = config['patching']['levels']
+        patching_levels = config["patching"]["levels"]
     except KeyError:
         patching_levels = 0
     if patching_levels:
-        data_channels *= (patching_levels + 1)
-    config_arch['in_channels'] = data_channels
+        data_channels *= patching_levels + 1
+    config_arch["in_channels"] = data_channels
 
     # Dispatch model creation
     try:
         return dispatch_model(MODEL_ZOO[arch], config_arch)
     except KeyError:
-        raise ValueError(f'Got config.arch={arch}, expected one of {MODEL_ZOO.keys}.')
+        raise ValueError(f"Got config.arch={arch}, expected one of {MODEL_ZOO.keys}.")
 
 
 def dispatch_model(ModelClass, config):
     """This function just creates an instance of the model ModelClass(**config)
     but performs additional checks to warn users about missing/wrong arguments.
-    
+
     Parameters
     ----------
     ModelClass : nn.Module
@@ -78,17 +78,24 @@ def dispatch_model(ModelClass, config):
     """
     sig = inspect.signature(ModelClass)
     model_name = ModelClass.__name__
-    
+
     # Verify that given parameters are actually arguments of the model
     for key in config:
         if key not in sig.parameters:
-            print(f"Given argument key={key} that is not in {model_name}'s signature.")
-            # warnings.warn(f"Given argument {key=} that is not in {model_name}'s signature.")
-    
+            print(f"Given argument key={key} "
+                  f"that is not in {model_name}'s signature.")
+            # warnings.warn(f"Given argument {key=} "
+            #               f"that is not in {model_name}'s signature.")
+
     # Check for model arguments not specified in the configuration
     for key, value in sig.parameters.items():
         if (value.default is not inspect._empty) and (key not in config):
-            print(f"Keyword argument {key} not specified for model {model_name}, using default={value.default}.")
-            # warnings.warn(f"Keyword argument {key} not specified for model {model_name}, using default={value.default}.")
+            print(
+                f"Keyword argument {key} not specified for model {model_name}, "
+                f"using default={value.default}."
+            )
+            # warnings.warn(
+            #     f"Keyword argument {key} not specified for model {model_name}, "
+            #     f"using default={value.default}.")
 
     return ModelClass(**config)
