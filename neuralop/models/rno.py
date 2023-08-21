bddf60fb0fb3fce509cb1a573f178599b11eff33
Miguel O. Liu-Schiaffini <mliuschi@caltech.edu>
2023-08-21 04:43:34 -0700

    Generalized RNO, still some padding bugs

--- neuralop/models/rno.py
+++ neuralop/models/rno.py
@@ -15,20 +15,32 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-from spectral_convolution import FactorizedSpectralConv2d
+from spectral_convolution import FactorizedSpectralConv1d, FactorizedSpectralConv2d, FactorizedSpectralConv3d
+from padding import DomainPadding
 
 torch.manual_seed(0)
 np.random.seed(0)
 
-class FourierLayer2d(nn.Module):
-    def __init__(self, modes1, modes2, width):
-        super(FourierLayer2d, self).__init__()
+class FourierLayer(nn.Module):
+    def __init__(self, n_modes, width, fft_norm='ortho', factorization=None, separable=False):
+        """
+            n_modes : tuple of modes
+            width : int of width of spectral convolution
+        """
+        super(FourierLayer, self).__init__()
 
-        self.modes1 = modes1
-        self.modes2 = modes2
         self.width = width
 
-        self.conv = FactorizedSpectralConv2d(self.width, self.width, (self.modes1, self.modes2), n_layers=1, fft_norm='ortho', factorization=None, separable=False)
+        if len(n_modes) == 1:
+            spec_conv = FactorizedSpectralConv1d
+        elif len(n_modes) == 2:
+            spec_conv = FactorizedSpectralConv2d
+        elif len(n_modes) == 3:
+            spec_conv = FactorizedSpectralConv3d
+        else:
+            raise NotImplementedError
+
+        self.conv = spec_conv(width, width, n_modes, n_layers=1, fft_norm=fft_norm, factorization=factorization, separable=separable)
         self.w = nn.Conv1d(self.width, self.width, 1)
 
     def forward(self, x):
@@ -40,19 +52,17 @@ class FourierLayer2d(nn.Module):
         return x1 + x2
 
 class RNO_cell(nn.Module):
-    def __init__(self, modes1, modes2, width):
+    def __init__(self, n_modes, width, fft_norm='ortho', factorization=None, separable=False):
         super(RNO_cell, self).__init__()
 
-        self.modes1 = modes1
-        self.modes2 = modes2
         self.width = width
 
-        self.f1 = FourierLayer2d(self.modes1, self.modes2, self.width)
-        self.f2 = FourierLayer2d(self.modes1, self.modes2, self.width)
-        self.f3 = FourierLayer2d(self.modes1, self.modes2, self.width)
-        self.f4 = FourierLayer2d(self.modes1, self.modes2, self.width)
-        self.f5 = FourierLayer2d(self.modes1, self.modes2, self.width)
-        self.f6 = FourierLayer2d(self.modes1, self.modes2, self.width)
+        self.f1 = FourierLayer(n_modes, self.width, fft_norm=fft_norm, factorization=factorization, separable=separable)
+        self.f2 = FourierLayer(n_modes, self.width, fft_norm=fft_norm, factorization=factorization, separable=separable)
+        self.f3 = FourierLayer(n_modes, self.width, fft_norm=fft_norm, factorization=factorization, separable=separable)
+        self.f4 = FourierLayer(n_modes, self.width, fft_norm=fft_norm, factorization=factorization, separable=separable)
+        self.f5 = FourierLayer(n_modes, self.width, fft_norm=fft_norm, factorization=factorization, separable=separable)
+        self.f6 = FourierLayer(n_modes, self.width, fft_norm=fft_norm, factorization=factorization, separable=separable)
 
         self.b1 = nn.Parameter(torch.normal(torch.tensor(0.),torch.tensor(1.))) # constant bias terms
         self.b2 = nn.Parameter(torch.normal(torch.tensor(0.),torch.tensor(1.)))
@@ -68,22 +78,21 @@ class RNO_cell(nn.Module):
         return h_next
 
 class RNO_layer(nn.Module):
-    def __init__(self, modes1, modes2, width, return_sequences=False):
+    def __init__(self, n_modes, width, return_sequences=False, fft_norm='ortho', factorization=None, separable=False):
         super(RNO_layer, self).__init__()
 
-        self.modes1 = modes1
-        self.modes2 = modes2
         self.width = width
         self.return_sequences = return_sequences
 
-        self.cell = RNO_cell(modes1, modes2, width)
+        self.cell = RNO_cell(n_modes, width, fft_norm=fft_norm, factorization=factorization, separable=separable)
         self.bias_h = nn.Parameter(torch.normal(torch.tensor(0.),torch.tensor(1.)))
 
     def forward(self, x, h=None):
-        batch_size, timesteps, dim, dom_size1, dom_size2 = x.shape
+        batch_size, timesteps, dim = x.shape[:3]
+        dom_sizes = x.shape[3:]
 
         if h is None:
-            h = torch.zeros((batch_size, self.width, dom_size1, dom_size2)).to(x.device)
+            h = torch.zeros((batch_size, self.width, *dom_sizes)).to(x.device)
             h += self.bias_h
 
         outputs = []
@@ -98,44 +107,67 @@ class RNO_layer(nn.Module):
             return h
 
 
-class RNO_2D(nn.Module):
-    def __init__(self, in_dim, out_dim, num_layers, modes1, modes2, width, pad_amount=None, pad_dim='1', use_grid=True):
+class RNO(nn.Module):
+    def __init__(self, in_dim, out_dim, 
+                num_layers, 
+                n_modes, 
+                width, 
+                residual=False, 
+                domain_padding=None, 
+                domain_padding_mode='one-sided', 
+                use_grid=True, 
+                output_scaling_factor=None,
+                fft_norm='ortho', 
+                factorization=None, 
+                separable=False
+                ):
         """
-            `pad_dim` can be '1', '2', or 'both', and this decides which of the two space dimensions to pad
-            `pad_amount` is a tuple that determines how much to pad each dimension by, if `pad_dim`
-            specifies that dimension should be padded.
-                `pad_amount` should be given as the fraction of the input resolution
+            General N-D RNO implementation:
+                `domain_padding` : float list, optional
+                    If not None, percentage of padding to use, by default None
+                    Must be a list
+                `domain_padding_mode` : {'symmetric', 'one-sided'}, optional
+                    How to perform domain padding, by default 'one-sided'
+                `residual` : whether to use residual connections in the hidden layers
+                `output_scaling_factor` : scaling factor of output resolution
         """
-        super(RNO_2D, self).__init__()
+        super(RNO, self).__init__()
 
-        self.modes1 = modes1
-        self.modes2 = modes2
+        self.n_modes = n_modes
+        self.n_dims = len(n_modes)
         self.num_layers = num_layers
         self.width = width
-        self.pad_amount = pad_amount # pads dom_size1 dimension
-        self.pad_dim = pad_dim
+
+        if domain_padding is not None and ((isinstance(domain_padding, list) and sum(domain_padding) > 0)):
+                domain_padding = [0,] + domain_padding # avoid padding channel dimension
+                self.domain_padding = DomainPadding(domain_padding=domain_padding, padding_mode=domain_padding_mode, output_scaling_factor=output_scaling_factor)
+        else:
+            self.domain_padding = None
+
+        self.domain_padding_mode = domain_padding_mode
+
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.use_grid = use_grid
+        self.residual = residual
 
         if use_grid:
-            self.lifting = nn.Linear(in_dim + 2, self.width)
+            self.lifting = nn.Linear(in_dim + len(n_modes), self.width)
         else:
             self.lifting = nn.Linear(in_dim, self.width)
 
-        module_list = [RNO_layer(modes1, modes2, width, return_sequences=True)
+        module_list = [RNO_layer(n_modes, width, return_sequences=True, fft_norm=fft_norm, factorization=factorization, separable=separable)
                                      for _ in range(num_layers - 1)]
-        module_list.append(RNO_layer(modes1, modes2, width, return_sequences=False))
+        module_list.append(RNO_layer(n_modes, width, return_sequences=False, fft_norm=fft_norm, factorization=factorization, separable=separable))
         self.layers = nn.ModuleList(module_list)
 
         self.projection = nn.Linear(self.width, out_dim)
     
     def forward(self, x, init_hidden_states=None): # h must be padded if using padding
-        batch_size, timesteps, dom_size1, dom_size2, dim = x.shape
-
-        if self.pad_amount:
-            dom_sizes = x.shape[2 : 2 + len(self.pad_amount)]
-            pad_size = [round(self.pad_amount[i] * dom_sizes[i]) for i in range(len(self.pad_amount))]
+        batch_size, timesteps = x.shape[:2]
+        dim = x.shape[-1]
+        dom_sizes = x.shape[2 : 2 + self.n_dims]
+        x_size = len(x.shape)
 
         if init_hidden_states is None:
             init_hidden_states = [None] * self.num_layers
@@ -146,42 +178,35 @@ class RNO_2D(nn.Module):
         
         x = self.lifting(x)
 
-        x = x.permute(0, 1, 4, 2, 3) # new shape: (batch, timesteps, dim, dom_size1, dom_size2)
-        if self.pad_amount: # pad the domain if input is non-periodic
-            if self.pad_dim == '1':
-                x = x.permute(0, 1, 2, 4, 3) # new shape: (batch, timesteps, dim, dom_size2, dom_size1)
-                x = F.pad(x, [0,pad_size[0]])
-                x = x.permute(0, 1, 2, 4, 3)
-            elif self.pad_dim == '2':
-                x = F.pad(x, [0,pad_size[1]])
-            elif self.pad_dim == 'both':
-                x = x.permute(0, 1, 2, 4, 3) # new shape: (batch, timesteps, dim, dom_size2, dom_size1)
-                x = F.pad(x, [0,pad_size[0]])
-                x = x.permute(0, 1, 2, 4, 3)
-                x = F.pad(x, [0,pad_size[1]])
+        x = torch.movedim(x, x_size - 1, 2) # new shape: (batch, timesteps, dim, dom_size1, dom_size2, ..., dom_sizen)
+
+        x = self.domain_padding.pad(x)
 
         final_hidden_states = []
         for i in range(self.num_layers):
             pred_x = self.layers[i](x, init_hidden_states[i])
-            # using residual predictions
             if i < self.num_layers - 1:
-                x = x + pred_x
+                if self.residual:
+                    x = x + pred_x
+                else:
+                    x = pred_x
                 final_hidden_states.append(x[:, -1])
             else:
                 x = pred_x
                 final_hidden_states.append(x)
         h = final_hidden_states[-1]
 
-        if self.pad_amount: # remove padding
-            if self.pad_dim == '1':
-                h = h[:, :, :-pad_size[0]]
-            elif self.pad_dim == '2':
-                h = h[..., :-pad_size[1]]
-            elif self.pad_dim == 'both':
-                h = h[:, :, :-pad_size[0]]
-                h = h[..., :-pad_size[1]]
+        h = h.unsqueeze(1) # add dim for padding compatibility
+        #print(self.domain_padding._unpad_indices[f"{list(h.shape[2:])}"])
+        h = self.domain_padding.unpad(h)
+        h = h[:,0] # remove extraneous dim
+        #print(h.shape)
 
-        h = h.permute(0, 2, 3, 1)
+        h = torch.movedim(h, 1, x_size - 2)
+        #print(h.shape)
+
+        #assert False
+
         pred = self.projection(h)
 
         return pred, final_hidden_states
@@ -193,18 +218,23 @@ class RNO_2D(nn.Module):
         for i in range(num_steps):
             pred, states = self.forward(x, states)
             output.append(pred)
-            x = pred.reshape((pred.shape[0], 1, pred.shape[1], pred.shape[2], pred.shape[3]))
+            x = pred.reshape((pred.shape[0], 1, *pred.shape[1:]))
 
         return torch.stack(output, dim=1)
 
     def get_grid(self, shape, device):
-        batchsize, steps, dom_size1, dom_size2, _ = shape
+        batch_size, steps = shape[:2]
+        dim = shape[-1]
+        dom_sizes = shape[2 : 2 + self.n_dims]
 
-        gridx = torch.tensor(np.linspace(0, 1, dom_size1), dtype=torch.float)
-        gridx = gridx.reshape(1, 1, dom_size1, 1, 1).repeat([batchsize, steps, 1, dom_size2, 1])
-        gridy = torch.tensor(np.linspace(0, 1, dom_size2), dtype=torch.float)
-        gridy = gridy.reshape(1, 1, 1, dom_size2, 1).repeat([batchsize, steps, dom_size1, 1, 1])
-        return torch.cat((gridx, gridy), dim=-1).to(device)
+        grids = []
+        for i in range(len(dom_sizes)):
+            size = dom_sizes[i]
+            grid = torch.tensor(np.linspace(0, 1, size), dtype=torch.float)
+            grid = grid.reshape([1 for j in range(2 + i)] + [size] + [1 for j in range(len(shape) - 3 - i)]).repeat([batchsize, steps] + shape[2 : 2 + i] + [1] + shape[3 + i : -1] + [1])
+            grids.append(grid)
+
+        return torch.cat(grids, dim=-1).to(device)
 
     def count_params(self):
         # Credit: Vadim Smolyakov on PyTorch forum
