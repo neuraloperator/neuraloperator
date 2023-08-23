67206e7153f8bc9b444461a1ac8468e3295ee2df
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2023-08-23 14:21:41 -0700

    Add and use 'fetch_npz' function

--- neuralop/datasets/burgers.py
+++ neuralop/datasets/burgers.py
@@ -1,19 +1,51 @@
 from pathlib import Path
+from typing import Tuple, Union
+
 import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+from neuralop.datasets.npz_dataset import fetch_npz
 
 
 def load_burgers(
-    data_path, n_train, n_test, batch_train=32, batch_test=100, time=1, grid=[0, 1]
-):
+    data_home: Union[str, Path],
+    file_name: str,
+    n_train,
+    n_test,
+    downsample_rate=8,
+    batch_train=32,
+    batch_test=100,
+    grid=(0, 1),
+) -> Tuple[DataLoader, DataLoader]:
+    """Loads the 1D Burgers dataset from an ``.npz`` data file.
 
-    data_path = Path(data_path).joinpath("burgers.pt").as_posix()
-    data = torch.load(data_path)
+    Target datafile will be read like a dict with initial (i.e. input) field
+    values ``a(x, t=0)`` at key "a" and final (i.e. output to learn) field
+    values ``u(x, t=1)`` at key "u".
 
-    x_train = data[0:n_train, :, 0]
-    x_test = data[n_train : (n_train + n_test), :, 0]
+    Parameters
+    ----------
+    data_home : str | PathLike[str]
+        Fully qualified path to a directory containing the target ``.npz`` file.
+    downsample_rate : int, optional (default=8)
+        How much data to skip (as in a list slice) in DataLoader when training
+        an operator. Ex: ``downsample_rate=1`` learns from all data, while
+        ``downsample_rate=10`` learns from 10% of the original data.
 
-    y_train = data[0:n_train, :, time]
-    y_test = data[n_train : (n_train + n_test), :, time]
+    Returns
+    -------
+    A tuple like ``(train_loader, test_loader)`` containing two
+    ``torch.utils.data.DataLoader``s
+    """
+    data = fetch_npz(file_name, "", data_home)  # unused zenodo_url
+    x_data: torch.Tensor = torch.tensor(data.a[:, :, ::downsample_rate])
+    y_data: torch.Tensor = torch.tensor(data.u[:, :, ::downsample_rate])
+
+    x_train = x_data[0:n_train, :, :]
+    x_test = x_data[n_train : (n_train + n_test), :, :]
+
+    y_train = y_data[0:n_train, :, :]
+    y_test = y_data[n_train : (n_train + n_test), :, :]
 
     s = x_train.size(-1)
 
@@ -23,16 +55,17 @@ def load_burgers(
         grid_train = grid.repeat(n_train, 1)
         grid_test = grid.repeat(n_test, 1)
 
-        x_train = torch.cat((x_train.unsqueeze(1), grid_train.unsqueeze(1)), 1)
-        x_test = torch.cat((x_test.unsqueeze(1), grid_test.unsqueeze(1)), 1)
+        # ``x_data`` is already unsqueezed
+        x_train = torch.cat((x_train, grid_train.unsqueeze(1)), 1)
+        x_test = torch.cat((x_test, grid_test.unsqueeze(1)), 1)
 
-    train_loader = torch.utils.data.DataLoader(
-        torch.utils.data.TensorDataset(x_train, y_train),
+    train_loader = DataLoader(
+        TensorDataset(x_train, y_train),
         batch_size=batch_train,
         shuffle=False,
     )
-    test_loader = torch.utils.data.DataLoader(
-        torch.utils.data.TensorDataset(x_test, y_test),
+    test_loader = DataLoader(
+        TensorDataset(x_test, y_test),
         batch_size=batch_test,
         shuffle=False,
     )
---
+++ neuralop/datasets/npz_dataset.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from typing import Optional, Union
+
+from numpy import load as np_load
+
+DEFAULT_DATA_HOME = Path.home().joinpath("neuraloperator_data")
+
+
+def _get_data_home(data_home: Optional[str, Path] = None) -> Path:
+    if data_home is None:
+        data_home = DEFAULT_DATA_HOME
+    if isinstance(data_home, str):
+        data_home = Path(data_home)
+
+    if not data_home.exists():
+        data_home.mkdir()
+
+    return data_home
+
+
+def fetch_npz(
+    file_name: str,
+    unused_zenodo_url: str,
+    data_home: Optional[Union[str, Path]] = None,
+):
+    """Loads a target ``.npz`` file.
+
+    This does not download the target file (though future versions of this
+    function will automatically download the specified file
+    if it's not on disk).
+
+    Parameters
+    ----------
+    data_home : str, default=None
+        Specify a download folder for the datasets. If None,
+        all neuraloperator data is stored in "~/neuraloperator_data" subfolders.
+    """
+    data_home = _get_data_home(data_home)
+    file_path = data_home / file_name
+    try:
+        return np_load(str(file_path))
+    except FileNotFoundError:
+        raise NotImplementedError(
+            f"File {file_path} not found on disk. "
+            f"Please make sure {file_name} is in the target directory "
+            f"and try again"
+        )
