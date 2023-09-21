5b5eb2475d23e7c24e45746bb1833f5b9db7653f
dhpitt <david.h.pitt@gmail.com>
2023-09-21 17:21:15 +0000

    CallbackPipeline

--- neuralop/training/callbacks.py
+++ neuralop/training/callbacks.py
@@ -7,6 +7,8 @@ logic of callbacks in Pytorch-Lightning (https://lightning.ai/docs/pytorch/stabl
 """
 
 import sys
+from typing import List
+
 import torch
 import wandb
 
@@ -85,7 +87,127 @@ class Callback(object):
     
     def on_val_end(self, *args, **kwargs):
         pass
+
+
+class CallbackPipeline(object):
+    def __init__(self, callbacks: List[Callback]):
+        self.callbacks = callbacks
+
+        overrides_device_load = [getattr(c, "on_load_to_device") != getattr(Callback, "on_load_to_device")\
+                                  for c in callbacks]
+        assert sum(overrides_device_load) <= 2, "More than one callback cannot override device loading"
+        if sum(overrides_device_load) == 1:
+            self.device_load_callback_idx = overrides_device_load.index(True)
+            print("using custom callback to load data to device.")
+        else:
+            self.device_load_callback_idx = None
+            print("using standard method to load data to device.")
+
+        # unless loss computation is overriden, call a basic loss function calculation
+        overrides_loss = [getattr(c, "compute_training_loss") != getattr(Callback, "compute_training_loss")\
+                                  for c in callbacks]
+        print(overrides_loss)
+
+        if sum(overrides_loss) >= 1:
+            self.overrides_loss = True
+            print("using custom callback to compute loss.")
+        else:
+            self.overrides_loss = False
+            print("using standard method to compute loss.")
+        
+    def _update_state_dict(self, **kwargs):
+        for c in self.callbacks:
+            c._update_state_dict(kwargs)
+
+    def on_init_start(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_init_start(*args, **kwargs)
+
+    def on_init_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_init_end(*args, **kwargs)
+
+    def on_before_train(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_before_train(*args, **kwargs)
+
+    def on_train_start(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_train_start(*args, **kwargs)
+
+    def on_epoch_start(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_epoch_start(*args, **kwargs)
     
+    def on_batch_start(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_batch_start(*args, **kwargs)
+
+    def on_load_to_device(self, *args, **kwargs):
+        if self.device_load_callback_idx:
+            self.callbacks[self.device_load_callback_idx].on_load_to_device(*args, *kwargs)
+    
+    def on_before_forward(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_before_forward(*args, **kwargs)
+
+    def on_before_loss(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_before_loss(*args, **kwargs)
+    
+    def compute_training_loss(self, *args, **kwargs):
+        if self.overrides_loss:
+            for c in self.callbacks:
+                c.compute_training_loss(*args, **kwargs)
+        else:
+            pass
+    
+    def on_batch_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_batch_end(*args, **kwargs)
+    
+    def on_epoch_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_epoch_end(*args, **kwargs)
+    
+    def on_train_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_train_end(*args, **kwargs)
+
+    def on_before_val(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_before_val(*args, **kwargs)
+
+    def on_val_epoch_start(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_val_epoch_start(*args, **kwargs)
+    
+    def on_val_batch_start(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_val_batch_start(*args, **kwargs)
+
+    def on_before_val_loss(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_before_val_loss(*args, **kwargs)
+    
+    def compute_val_loss(self, *args, **kwargs):
+        if self.overrides_loss:
+            for c in self.callbacks:
+                c.compute_val_loss(*args, **kwargs)
+        else:
+            pass
+    
+    def on_val_batch_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_val_batch_end(*args, **kwargs)
+
+    def on_val_epoch_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_val_epoch_end(*args, **kwargs)
+    
+    def on_val_end(self, *args, **kwargs):
+        for c in self.callbacks:
+            c.on_val_end(*args, **kwargs)
 
 class SimpleWandBLoggerCallback(Callback):
     """
