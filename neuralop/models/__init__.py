358983cca78d16bdb76c36614e6cc191544e1a2c
Jean Kossaifi <jean.kossaifi@gmail.com>
2023-09-30 21:16:21 -0700

    models: don't import fnogno by default to avoid torch_scatter
    Avoid forced dependency on torch_scatter

--- neuralop/models/__init__.py
+++ neuralop/models/__init__.py
@@ -2,5 +2,5 @@ from .fno import TFNO, TFNO1d, TFNO2d, TFNO3d
 from .fno import FNO, FNO1d, FNO2d, FNO3d
 from .fno import SFNO
 from .uno import UNO
-from .fnogno import FNOGNO
+# from .fnogno import FNOGNO
 from .model_dispatcher import get_model
