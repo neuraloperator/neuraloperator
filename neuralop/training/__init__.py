d7af2fde95cb3c79a034f2290c891ace2d24ce78
dhpitt <david.h.pitt@gmail.com>
2023-10-16 14:42:39 -0700

    add model checkpoint to callbacks

--- neuralop/training/__init__.py
+++ neuralop/training/__init__.py
@@ -1,4 +1,4 @@
 from .losses import LpLoss, H1Loss
 from .trainer import Trainer
 from .torch_setup import setup
-from .callbacks import Callback, MGPatchingCallback, OutputEncoderCallback, SimpleWandBLoggerCallback
+from .callbacks import Callback, MGPatchingCallback, OutputEncoderCallback, SimpleWandBLoggerCallback, ModelCheckpointCallback
