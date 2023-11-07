2bd018c85941352682044444b8f2be1c82ff8c84
cmu-mail-3090 <mengxinjlee@gmail.com>
2023-11-07 15:45:12 -0500

    fix spotted typos, and further refurnish some variable names and comments

--- neuralop/layers/attention_kernel_integral.py
+++ neuralop/layers/attention_kernel_integral.py
@@ -14,16 +14,20 @@ class AttentionKernelIntegral(torch.nn.Module):
 
     Parameters
     ----------
-    channels : int, input and output channels
-    n_heads : int, number of attention n_heads
+    in_channels : int, input channels
+    out_channels : int, output channels
+    n_heads : int, number of attention heads in multi-head attention
     head_n_channels : int, dimension of each attention head, determines how many function bases to use for the kernel
-    pos_dim : int, dimension of domain
-    use_pe : bool, whether to use positional encoding
+                      k(x, y) = \sum_{c=1}^d \q_c(x) * \k_c(y), head_n_channels controls the d
+    pos_dim : int, dimension of the domain, determines the dimension of coordinates
+    use_positional_encoding : bool, whether to use positional encoding to encode query and key
     project_query : bool, whether to project the query function with pointwise linear layer
+                   (this is sometimes not needed when using cross-attention)
     """
 
     def __init__(self,
-                 channels,
+                 in_channels,
+                 out_channels,
                  n_heads,
                  head_n_channels,
                  pos_dim,
@@ -37,18 +41,19 @@ class AttentionKernelIntegral(torch.nn.Module):
 
         self.project_query = project_query
         if project_query:
-            self.to_q = nn.Linear(channels, head_n_channels * n_heads, bias=False)
+            self.to_q = nn.Linear(in_channels, head_n_channels * n_heads, bias=False)
         else:
             self.to_q = nn.Identity()
 
-        self.to_k = nn.Linear(channels, head_n_channels * n_heads, bias=False)
+        self.to_k = nn.Linear(in_channels, head_n_channels * n_heads, bias=False)
 
         self.k_norm = nn.InstanceNorm1d(head_n_channels, affine=False)
         self.v_norm = nn.InstanceNorm1d(head_n_channels, affine=False)
 
-        self.to_v = nn.Linear(channels, head_n_channels * n_heads, bias=False)
+        self.to_v = nn.Linear(in_channels, head_n_channels * n_heads, bias=False)
 
-        self.to_out = nn.Linear(head_n_channels * n_heads, channels) if head_n_channels * n_heads != channels else nn.Identity()
+        self.to_out = nn.Linear(head_n_channels * n_heads, out_channels) \
+            if head_n_channels * n_heads != out_channels else nn.Identity()
 
         self.use_positional_encoding = use_positional_encoding
         self.pos_dim = pos_dim
@@ -58,11 +63,14 @@ class AttentionKernelIntegral(torch.nn.Module):
         self.initialize_qkv_weights()
 
     def init_weight(self, weight, inif_fn):
-        # initialization for the projection matrix
-        # basically initialize the weights for each n_heads
-        # to add the diagonal bias, it requires input dim = head_n_channels
-        # W = init_fn(W) + I * diagonal_weight
-        # init_fn is usually xavier_uniform_
+        """
+        Initialization for the projection matrix
+        basically initialize the weights for each heads with predefined initialization function and gain,
+        to add the diagonal bias, it requires input channels = head_n_channels
+        W = init_fn(W) + I * diagonal_weight
+
+        init_fn is xavier_uniform_ by default
+        """
 
         for param in weight.parameters():
             if param.ndim > 1:
@@ -73,6 +81,11 @@ class AttentionKernelIntegral(torch.nn.Module):
                         param.data[h * self.head_n_channels:(h + 1) * self.head_n_channels, :] += diagonal_bias
 
     def initialize_qkv_weights(self):
+        """
+        Initialize the weights for q, k, v projection matrix with a small gain and add a diagonal bias,
+        this technique has been found useful for scale-sensitive problem that has not been normalized
+        see Table 8 in https://arxiv.org/pdf/2105.14995.pdf
+        """
         init_fn = xavier_uniform_
 
         if self.project_query:
@@ -81,6 +94,10 @@ class AttentionKernelIntegral(torch.nn.Module):
         self.init_weight(self.to_v, init_fn)
 
     def normalize_wrt_domain(self, u, norm_fn):
+        """
+        Normalize the input function with respect to the domain
+        The second dimension is equal to the number of grid points that discretize the domain
+        """
         # u: the input or transformed function
         batch_size = u.shape[0]
         u = u.view(batch_size*self.n_heads, -1, self.head_n_channels)
@@ -90,12 +107,12 @@ class AttentionKernelIntegral(torch.nn.Module):
     def forward(self,
                 u_x,
                 pos_x,
-                pos_emb=None,    # positional encoding module for encoding q/k
+                positional_embedding_module=None,    # positional encoding module for encoding q/k
                 u_y=None,
                 pos_y=None,
                 weights=None,
                 associative=True,   # can be much faster if num_grid_points is larger than the channel number c
-                get_kernel=False):
+                return_kernel=False):
         """
         Computes kernel integral transform with attention
 
@@ -103,7 +120,7 @@ class AttentionKernelIntegral(torch.nn.Module):
         ----------
         u_x: input (query) function of shape [batch_size, num_grid_points, channels]
         pos_x: coordinate of input function's grid points [batch_size, num_grid_points, pos_dim]
-        pos_emb: positional encoding module for encoding query/key (q/k), a torch.nn.Module
+        positional_embedding_module: positional embedding module for encoding query/key (q/k), a torch.nn.Module
         u_y: the second source of function (key and value), if not provided, u_y = u_x
         pos_y: coordinate of the second source of function's grid points, if not provided, assume pos_y = pos_x
         weights : tensor of shape [batch_size, num_grid_points], if not provided assume to be 1/num_grid_points
@@ -111,7 +128,7 @@ class AttentionKernelIntegral(torch.nn.Module):
                   volume around f(y)=u_y W_v being integrated.
         associative: if True, use associativity of matrix multiplication, first multiply K^T V, then multiply Q,
                    much faster when num_grid_points is larger than the channel number (which is usually the case)
-        get_kernel: if True, return the kernel matrix (for analyzing the kernel)
+        return_kernel: if True, return the kernel matrix (for analyzing the kernel)
 
         Output
         ----------
@@ -121,7 +138,7 @@ class AttentionKernelIntegral(torch.nn.Module):
         if u_y is None:
             u_y = u_x   # go back to self attention
 
-        if get_kernel and associative:
+        if return_kernel and associative:
             raise Exception('Cannot get kernel matrix when associative is set to True')
 
         batch_size, num_grid_points = u_y.shape[:2]   # batch size and number of grid points
@@ -142,11 +159,11 @@ class AttentionKernelIntegral(torch.nn.Module):
         k = self.normalize_wrt_domain(k, self.k_norm)
         v = self.normalize_wrt_domain(v, self.v_norm)
 
-        if pos_emb is not None:
+        if positional_embedding_module is not None:
             if self.pos_dim == 2:
                 assert pos_x.shape[-1] == 2
-                q_freqs_x = pos_emb.forward(pos_x[..., 0], q.device)
-                q_freqs_y = pos_emb.forward(pos_x[..., 1], q.device)
+                q_freqs_x = positional_embedding_module.forward(pos_x[..., 0], q.device)
+                q_freqs_y = positional_embedding_module.forward(pos_x[..., 1], q.device)
                 q_freqs_x = q_freqs_x.unsqueeze(1).repeat([1, self.n_heads, 1, 1])
                 q_freqs_y = q_freqs_y.unsqueeze(1).repeat([1, self.n_heads, 1, 1])
 
@@ -154,27 +171,27 @@ class AttentionKernelIntegral(torch.nn.Module):
                     k_freqs_x = q_freqs_x
                     k_freqs_y = q_freqs_y
                 else:
-                    k_freqs_x = pos_emb.forward(pos_y[..., 0], k.device)
-                    k_freqs_y = pos_emb.forward(pos_y[..., 1], k.device)
+                    k_freqs_x = positional_embedding_module.forward(pos_y[..., 0], k.device)
+                    k_freqs_y = positional_embedding_module.forward(pos_y[..., 1], k.device)
                     k_freqs_x = k_freqs_x.unsqueeze(1).repeat([1, self.n_heads, 1, 1])
                     k_freqs_y = k_freqs_y.unsqueeze(1).repeat([1, self.n_heads, 1, 1])
 
-                q = pos_emb.apply_2d_rotary_pos_emb(q, q_freqs_x, q_freqs_y)
-                k = pos_emb.apply_2d_rotary_pos_emb(k, k_freqs_x, k_freqs_y)
+                q = positional_embedding_module.apply_2d_rotary_positional_embedding_module(q, q_freqs_x, q_freqs_y)
+                k = positional_embedding_module.apply_2d_rotary_positional_embedding_module(k, k_freqs_x, k_freqs_y)
             elif self.pos_dim == 1:
                 assert pos_x.shape[-1] == 1
 
-                q_freqs = pos_emb.forward(pos_x[..., 0], q.device)
+                q_freqs = positional_embedding_module.forward(pos_x[..., 0], q.device)
                 q_freqs = q_freqs.unsqueeze(1).repeat([batch_size, self.n_heads, 1, 1])
 
                 if pos_y is None:
                     k_freqs = q_freqs
                 else:
-                    k_freqs = pos_emb.forward(pos_y[..., 0], k.device)
+                    k_freqs = positional_embedding_module.forward(pos_y[..., 0], k.device)
                     k_freqs = k_freqs.unsqueeze(1).repeat([batch_size, self.n_heads, 1, 1])
 
-                q = pos_emb.apply_rotary_pos_emb(q, q_freqs)
-                k = pos_emb.apply_rotary_pos_emb(k, k_freqs)
+                q = positional_embedding_module.apply_rotary_pos_emb(q, q_freqs)
+                k = positional_embedding_module.apply_rotary_pos_emb(k, k_freqs)
             else:
                 raise Exception('Currently doesnt support relative embedding >= 3 dimensions')
 
@@ -188,7 +205,7 @@ class AttentionKernelIntegral(torch.nn.Module):
 
         u = u.permute(0, 2, 1, 3).contiguous().view(batch_size, num_grid_points, self.n_heads*self.head_n_channels)
         u = self.to_out(u)
-        if get_kernel:
+        if return_kernel:
             return u, kxy
         return u
 
