0862e40168a9702aa5e148fd3e83a41d6b164fe1
dhpitt <david.h.pitt@gmail.com>
2023-12-07 18:48:16 +0000

    assert eval error after resuming falls within margin

--- neuralop/training/tests/test_callbacks.py
+++ neuralop/training/tests/test_callbacks.py
@@ -27,7 +27,7 @@ class DummyModel(BaseModel, name='Dummy'):
     Simple linear model to mock-up our model API
     """
 
-    def __init__(self, features):
+    def __init__(self, features, **kwargs):
         super().__init__()
         self.net = nn.Linear(features, 1)
 
@@ -118,7 +118,7 @@ def test_model_checkpoint_and_resume():
                           ]
     )
 
-    trainer.train(train_loader=train_loader, 
+    errors = trainer.train(train_loader=train_loader, 
                   test_loaders={'': test_loader}, 
                   optimizer=optimizer,
                   scheduler=scheduler,
@@ -126,4 +126,55 @@ def test_model_checkpoint_and_resume():
                   training_loss=l2loss,
                   eval_losses=eval_losses,
                   )
+    
+    
+# ensure that model accuracy after loading from checkpoint
+# is comparable to accuracy at time of save
+def test_load_from_checkpoint():
+    model = DummyModel(50)
 
+    train_loader = DataLoader(DummyDataset(100))
+    test_loader = DataLoader(DummyDataset(20))
+
+    trainer = Trainer(model=model,
+                      n_epochs=5,
+                      callbacks=[
+                          CheckpointCallback(save_dir='./full_states',
+                                                  save_optimizer=True,
+                                                  save_scheduler=True,
+                                                  save_best='h1') # monitor h1 loss
+                      ]
+    )
+
+    optimizer = torch.optim.Adam(model.parameters(), 
+                                lr=8e-3, 
+                                weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
+
+    # Creating the losses
+    l2loss = LpLoss(d=2, p=2)
+    h1loss = H1Loss(d=2)
+
+    eval_losses={'h1': h1loss, 'l2': l2loss}
+
+    orig_model_eval_errors = trainer.train(train_loader=train_loader, 
+                  test_loaders={'': test_loader}, 
+                  optimizer=optimizer,
+                  scheduler=scheduler,
+                  regularizer=None,
+                  training_loss=l2loss,
+                  eval_losses=eval_losses
+                  )
+    
+    # create a new model from saved checkpoint and evaluate
+    loaded_model = DummyModel.from_checkpoint(save_folder='./full_states', save_name='best_model')
+    trainer = Trainer(model=loaded_model,
+                      n_epochs=1,
+    )
+
+    loaded_model_eval_errors = trainer.evaluate(loss_dict=eval_losses,
+                              data_loader=test_loader)
+
+    # log prefix is empty except for default underscore
+    assert orig_model_eval_errors['_h1'] - loaded_model_eval_errors['_h1'] < 0.1
+    
