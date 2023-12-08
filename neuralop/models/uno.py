20be3bd7b6f871e1e20490ed5aa64994639c93b5
Bahareh Tolooshams <btolooshams@gmail.com>
2023-12-08 13:44:18 -0800

    fixing the horizontal_skips_map construction, it was not going through (#267)
    * leaving output_scaling_factor to get list of list for uno_scaling in uno, and modify validate_scaling_factor to accept list of list

--- neuralop/models/uno.py
+++ neuralop/models/uno.py
@@ -172,11 +172,11 @@ class UNO(nn.Module):
         if self.horizontal_skips_map is None:
             self.horizontal_skips_map = {}
             for i in range(
-                n_layers // 2,
                 0,
-            ):
+                n_layers // 2,
+            ):  
+                # example, if n_layers = 5, then 4:0, 3:1
                 self.horizontal_skips_map[n_layers - i - 1] = i
-
         # self.uno_scalings may be a 1d list specifying uniform scaling factor at each layer
         # or a 2d list, where each row specifies scaling factors along each dimention.
         # To get the final (end to end) scaling factors we need to multiply
@@ -291,6 +291,7 @@ class UNO(nn.Module):
         skip_outputs = {}
         cur_output = None
         for layer_idx in range(self.n_layers):
+            
             if layer_idx in self.horizontal_skips_map.keys():
                 skip_val = skip_outputs[self.horizontal_skips_map[layer_idx]]
                 output_scaling_factors = [
@@ -309,8 +310,10 @@ class UNO(nn.Module):
             if layer_idx in self.horizontal_skips_map.values():
                 skip_outputs[layer_idx] = self.horizontal_skips[str(layer_idx)](x)
 
+
         if self.domain_padding is not None:
             x = self.domain_padding.unpad(x)
+        
 
         x = self.projection(x)
         return x
