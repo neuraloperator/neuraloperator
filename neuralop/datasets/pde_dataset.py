ed3565f292ba69904b255a095e95a35e09c05825
dhpitt <david.h.pitt@gmail.com>
2024-02-29 14:04:56 -0800

    finish boilerplate pde dataset

--- neuralop/datasets/pde_dataset.py
+++ neuralop/datasets/pde_dataset.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractproperty
 
 import torch
 from torch.utils.data import DataLoader, Dataset
+from neuralop.datasets import DataProcessor
 
 class PDEDataset(ABCMeta):
     def __init__(self):
@@ -19,30 +20,40 @@ class PDEDataset(ABCMeta):
         train_db: torch.utils.data.Dataset of training examples
         test_db:  ""                       of test examples
         train_loader: torch.utils.data.DataLoader for single-node, single-GPU training
-        test_loader: torch.utils.data.DataLoader for single-node, single-GPU training
+        test_loaders: torch.utils.data.DataLoader (s) for single-node, single-GPU training
         data_processor: neuralop.datasets.DataProcessor to process data examples
             optional, default is None
         """
         pass
     
     @abstractproperty
-    def train_db(self):
+    def train_db(self) -> Dataset:
         pass
     
     @abstractproperty
-    def test_db(self):
+    def test_db(self) -> Dataset:
         pass
     
     @abstractproperty
-    def train_loader(self):
+    def train_loader(self) -> DataLoader:
         pass
     
     @abstractproperty
-    def test_loader(self):
+    def test_loaders(self) -> DataLoader:
         pass
     
-    def data_processor(self):
+    def data_processor(self) -> DataProcessor:
         # Optional: provides a DataProcessor if overriden
         return None
     
+def to_PDEDataset(train_db, test_db, data_processor=None) -> PDEDataset:
+    """to_PDEDataset provides a simple way to turn torch datasets 
+        into the format of a PDEDataset
 
+    Parameters
+    ----------
+    train_db : torch.utils.data.Dataset
+    test_db : torch.utils.data.Dataset
+    data_processor : neuralop.datasets.DataProcessor, optional
+        default is None
+    """
