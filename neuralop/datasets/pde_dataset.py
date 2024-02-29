2359a56fae1f76ef0a78c622e2621ea13e990241
dhpitt <david.h.pitt@gmail.com>
2024-02-29 13:00:41 -0800

    rename to PDEDataset and add abstractprops

--- neuralop/datasets/pde_dataset.py
+++ neuralop/datasets/pde_dataset.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractproperty
 
 import torch
 from torch.utils.data import DataLoader, Dataset
@@ -9,12 +9,40 @@ class PDEDataset(ABCMeta):
             Datasets contain input-output pairs a(x), u(x) and may also
             contain additional information, e.g. function parameters,
             input geometry or output query points.
+
+            datasets may implement a download flag at init, which provides
+            access to a number of premade datasets for sample problems provided
+            in our Zenodo archive. 
+
         All datasets are required to expose the following attributes after init:
 
         train_db: torch.utils.data.Dataset of training examples
         test_db:  ""                       of test examples
         train_loader: torch.utils.data.DataLoader for single-node, single-GPU training
         test_loader: torch.utils.data.DataLoader for single-node, single-GPU training
-        data_processor: neuralop.datasets.DataProcessor 
+        data_processor: neuralop.datasets.DataProcessor to process data examples
+            optional, default is None
         """
+        pass
+    
+    @abstractproperty
+    def train_db(self):
+        pass
+    
+    @abstractproperty
+    def test_db(self):
+        pass
+    
+    @abstractproperty
+    def train_loader(self):
+        pass
+    
+    @abstractproperty
+    def test_loader(self):
+        pass
+    
+    def data_processor(self):
+        # Optional: provides a DataProcessor if overriden
+        return None
+    
 
