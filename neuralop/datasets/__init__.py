925181b6c7b9e9e2d7b38f2b6e8691e88abaea70
dhpitt <david.h.pitt@gmail.com>
2024-03-01 15:27:09 -0800

    refactor to combine darcy and navier stokes b/c common format

--- neuralop/datasets/__init__.py
+++ neuralop/datasets/__init__.py
@@ -1,6 +1,5 @@
-from .darcy import DarcyFlowDataset #, SmallDarcyFlowDataset
 from .spherical_swe import load_spherical_swe
-from .navier_stokes import NavierStokesDataset 
 from .pt_dataset import load_pt_traintestsplit
+from .pde_dataset import DarcyFlowDataset, NavierStokesDataset
 from .burgers import load_burgers_1dtime
 from .data_transforms import DataProcessor
