8ee7824d2c000d34d09b2ce74c13591eb742e7cb
dhpitt <david.h.pitt@gmail.com>
2024-03-05 11:11:44 -0800

    only import MeshDataModule if open3d is built

--- neuralop/datasets/__init__.py
+++ neuralop/datasets/__init__.py
@@ -4,4 +4,8 @@ from .navier_stokes import load_navier_stokes_pt
 from .pt_dataset import load_pt_traintestsplit
 from .burgers import load_burgers_1dtime
 from .dict_dataset import DictDataset
-from .mesh_datamodule import MeshDataModule
+
+# only import MeshDataModule if open3d is built locally
+from importlib.util import find_spec
+if find_spec('open3d') is not None:
+    from .mesh_datamodule import MeshDataModule
