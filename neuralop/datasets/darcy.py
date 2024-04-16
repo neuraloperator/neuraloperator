84a5f36b44556e908feea5d1abde4d35c266d6e9
dhpitt <david.h.pitt@gmail.com>
2024-04-16 15:52:27 -0700

    first try new darcy with zenodo

--- neuralop/datasets/darcy.py
+++ neuralop/datasets/darcy.py
@@ -1,36 +1,69 @@
 
 import torch
-from .pde_dataset import BasePDEDataset, partialclass
+from pathlib import Path
+from typing import Union, List
 
-    
-'''def train_loader(self, 
-                    num_workers: int=None, 
-                    pin_memory: bool=True,
-                    persistent_workers: bool=False) -> DataLoader:
-    
-    return DataLoader(dataset=self.train_db,
-                        batch_size=self.batch_size,
-                        shuffle=True,
-                        num_workers=num_workers,
-                        pin_memory=pin_memory,
-                        persistent_workers=persistent_workers,
-                        )
+from .pt_dataset import PTDataset
+from .web_utils import download_from_url
 
-def test_loaders(self, 
-                    num_workers: int=None, 
-                    pin_memory: bool=True,
-                    persistent_workers: bool=False) -> Dict[DataLoader]:
-    test_loaders = {}
-    for (res, batch_size) in zip(self.test_resolutions, self.test_batch_sizes):
-        loader = DataLoader(dataset=self.test_dbs[res],
-                            batch_size=batch_size,
-                            shuffle=False,
-                            num_workers=num_workers,
-                            pin_memory=pin_memory,
-                            persistent_workers=persistent_workers,
-                            )
-        test_loaders[res] = loader
-return test_loaders        '''
+class DarcyDataset(PTDataset):
+    def __init__(self,
+                 root_dir: Union[Path, str],
+                 dataset_name: str,
+                 n_train: int,
+                 n_tests: List[int],
+                 batch_size: int,
+                 test_batch_sizes: List[int],
+                 train_resolution: int,
+                 test_resolutions: int=[16,32],
+                 grid_boundaries: List[int]=[[0,1],[0,1]],
+                 positional_encoding: bool=True,
+                 encode_input: bool=False, 
+                 encode_output: bool=True, 
+                 encoding="channel-wise",
+                 channel_dim=1,
+                 download: bool=True):
 
-# Load small darcy flow as a partial class of DarcyFlowDataset
-#SmallDarcyFlowDataset = partialclass(DarcyFlowDataset, train_resolution=16)
+        # url/md5 info for data hosted on Zenodo archive
+        dataset_info = {
+            16: {
+                'train': {
+                    "url": "https://zenodo.org/records/10982484/files/darcy_train_16.pt?download=1",
+                    "md5": "248e3c55c8c4b5a41ff2b972bf56c7d9"
+                },
+                'test': {
+                    "url": "https://zenodo.org/records/10982484/files/darcy_test_16.pt?download=1",
+                    "md5": "9f747d431dc5fd91b5bff2dd580ae452"
+                },
+            }
+        }
+
+        # download darcy data from zenodo archive if passed
+        if download:
+            train_info = dataset_info[train_resolution]["train"]
+            download_from_url(url=train_info["url"], 
+                              md5=train_info["md5"],
+                              root=root_dir,
+                              filename=f"darcy_train_{train_resolution}.pt")
+            for test_res in test_resolutions:
+                test_info = dataset_info[test_res]["test"]
+                download_from_url(url=test_info["url"], 
+                                md5=test_info["md5"],
+                                root=root_dir,
+                                filename=f"darcy_test_{test_res}.pt")
+        
+        # once downloaded/if files already exist, init PTDataset
+        super.__init__(root_dir=root_dir,
+                       dataset_name="darcy",
+                       n_train=n_train,
+                       n_tests=n_tests,
+                       batch_size=batch_size,
+                       test_batch_sizes=test_batch_sizes,
+                       train_resolution=train_resolution,
+                       test_resolutions=test_resolutions,
+                       grid_boundaries=grid_boundaries,
+                       positional_encoding=positional_encoding,
+                       encode_input=encode_input,
+                       encode_output=encode_output,
+                       encoding=encoding,
+                       channel_dim=channel_dim,)
