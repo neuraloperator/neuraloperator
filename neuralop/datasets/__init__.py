8b4a178e1eb506108a846779c0d9766e1466097c
dhpitt <david.h.pitt@gmail.com>
2024-04-18 13:48:36 -0700

    export proper names from datasets module

--- neuralop/datasets/__init__.py
+++ neuralop/datasets/__init__.py
@@ -1,6 +1,6 @@
 from .spherical_swe import load_spherical_swe
-from .pt_dataset import load_pt_traintestsplit
-from .pde_dataset import DarcyFlowDataset, NavierStokesDataset
+from .pt_dataset import PTDataset
+from .darcy import DarcyDataset
 from .burgers import load_burgers_1dtime
 from .dict_dataset import DictDataset
 from .data_transforms import DataProcessor
