c5ff454a4f8ad5753f6e6afe1e9ae6b24b6c95da
dhpitt <david.h.pitt@gmail.com>
2024-05-17 06:54:42 -0700

    add flag for map location in load_from_checkpoint

--- neuralop/models/base_model.py
+++ neuralop/models/base_model.py
@@ -93,10 +93,10 @@ class BaseModel(torch.nn.Module):
     def load_checkpoint(self, save_folder, save_name, map_location=None):
         save_folder = Path(save_folder)
         state_dict_filepath = save_folder.joinpath(f'{save_name}_state_dict.pt').as_posix()
-        self.load_state_dict(torch.load(state_dict_filepath), map_location=map_location)
+        self.load_state_dict(torch.load(state_dict_filepath, map_location=map_location))
     
     @classmethod
-    def from_checkpoint(cls, save_folder, save_name):
+    def from_checkpoint(cls, save_folder, save_name, map_location=None):
         save_folder = Path(save_folder)
 
         metadata_filepath = save_folder.joinpath(f'{save_name}_metadata.pkl').as_posix()
@@ -115,7 +115,7 @@ class BaseModel(torch.nn.Module):
             init_args = []
         instance = cls(*init_args, **init_kwargs)
 
-        instance.load_checkpoint(save_folder, save_name)
+        instance.load_checkpoint(save_folder, save_name, map_location=map_location)
         return instance
 
 
