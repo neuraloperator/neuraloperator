073d53b90248cf78bb9f8f8b482f44240a0360dc
dhpitt <david.h.pitt@gmail.com>
2024-05-22 13:42:40 -0700

    more granular mean and std testing:

--- neuralop/datasets/tests/test_output_encoder.py
+++ neuralop/datasets/tests/test_output_encoder.py
@@ -3,9 +3,9 @@ import torch
 from torch.testing import assert_close
 from flaky import flaky
 
-@flaky(max_runs=4, min_passes=3)
+#@flaky(max_runs=4, min_passes=3)
 def test_UnitGaussianNormalizer_created_from_stats(eps=1e-6):
-    x = torch.rand(16, 3, 40, 50, 60)*2.5
+    x = torch.rand(16, 3, 4, 5, 6)*2.5
     mean = torch.mean(x, dim=[0, 2, 3, 4], keepdim=True)
     std = torch.std(x, dim=[0, 2, 3, 4], keepdim=True)
 
@@ -18,14 +18,17 @@ def test_UnitGaussianNormalizer_created_from_stats(eps=1e-6):
     assert torch.mean(x_normalized) <= eps
     assert (torch.std(x_normalized) - 1) <= eps
 
-@flaky(max_runs=4, min_passes=3)
+#@flaky(max_runs=4, min_passes=3)
 def test_UnitGaussianNormalizer_from_data(eps=1e-6):
-    x = torch.rand(16, 3, 40, 50, 60)*2.5
+    x = torch.rand(16, 3, 4, 5, 6)*2.5
     mean = torch.mean(x, dim=[0, 2, 3, 4], keepdim=True)
     std = torch.std(x, dim=[0, 2, 3, 4], keepdim=True)   
     # Init by fitting whole data at once
     normalizer = UnitGaussianNormalizer(dim=[0, 2, 3, 4], eps=eps)
     normalizer.fit(x)
+    assert_close(normalizer.mean, mean)
+    assert_close(normalizer.std, std, rtol=eps, atol=eps)
+
     x_normalized = normalizer.transform(x)
     x_unnormalized = normalizer.inverse_transform(x_normalized)
 
@@ -33,10 +36,9 @@ def test_UnitGaussianNormalizer_from_data(eps=1e-6):
     assert torch.mean(x_normalized) <= eps
     assert (torch.std(x_normalized) - 1) <= eps
 
-    assert_close(normalizer.mean, mean)
-    assert_close(normalizer.std, std, rtol=eps, atol=eps)
+    
 
-@flaky(max_runs=4, min_passes=3)
+#@flaky(max_runs=4, min_passes=3)
 def test_UnitGaussianNormalizer_incremental_update(eps=1e-6):
     x = torch.rand(16, 3, 40, 50, 60)*2.5
     mean = torch.mean(x, dim=[0, 2, 3, 4], keepdim=True)
@@ -52,6 +54,5 @@ def test_UnitGaussianNormalizer_incremental_update(eps=1e-6):
     assert (torch.std(x_normalized) - 1) <= eps
 
     assert_close(normalizer.mean, mean)
-    print(normalizer.std, std)
     assert_close(normalizer.std, std, rtol=eps, atol=eps)
 
