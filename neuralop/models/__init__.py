d154725dd83ef62b00fbc23759a75b59a7d0661d
dhpitt <david.h.pitt@gmail.com>
2024-05-30 12:22:49 -0700

    make uqno available for import

--- neuralop/models/__init__.py
+++ neuralop/models/__init__.py
@@ -2,5 +2,6 @@ from .fno import TFNO, TFNO1d, TFNO2d, TFNO3d
 from .fno import FNO, FNO1d, FNO2d, FNO3d
 from .fno import SFNO
 from .uno import UNO
+from .uqno import UQNO
 from .fnogno import FNOGNO
 from .base_model import get_model
