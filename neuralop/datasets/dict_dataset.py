773db738addf15e8bdb3c910402436e6211ca3be
David Pitt <david.h.pitt@gmail.com>
2024-06-28 15:30:29 -0700

    fix deprecation msgs to str format (#354)

--- neuralop/datasets/dict_dataset.py
+++ neuralop/datasets/dict_dataset.py
@@ -1,5 +1,4 @@
 from tensorly.utils import DefineDeprecated
-from neuralop.data.datasets import dict_dataset
 
 warning_msg = "Warning: neuralop.datasets.dict_dataset is deprecated and has been moved to neuralop.data.datasets.dict_dataset."
-DictDataset = DefineDeprecated(dict_dataset.DictDataset, warning_msg)
+DictDataset = DefineDeprecated('neuralop.data.datasets.dict_dataset.DictDataset', warning_msg)
--- neuralop/datasets/tensor_dataset.py
+++ neuralop/datasets/tensor_dataset.py
@@ -1,5 +1,4 @@
 from tensorly.utils import DefineDeprecated
-from neuralop.data.datasets import tensor_dataset
 
 warning_msg = "Warning: neuralop.datasets.tensor_dataset is deprecated and has been moved to neuralop.data.datasets.tensor_dataset."
-TensorDataset = DefineDeprecated(tensor_dataset.TensorDataset, warning_msg)
+TensorDataset = DefineDeprecated('neuralop.data.datasets.tensor_dataset.TensorDataset', warning_msg)
