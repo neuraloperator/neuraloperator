88df5a10000472e3a3395d67efd9fb151fad835b
David Pitt <david.h.pitt@gmail.com>
2024-07-08 08:03:49 -0700

    Update datasets to include download, dataset metaclass, dataset objects (#299)
    * boilerplate dataset and dataProcessor in __init__
    
    * rename to PDEDataset and add abstractprops
    
    * starter of boilerplate for darcy
    
    * finish boilerplate pde dataset
    
    * Darcy Flow pdeDataset WIP
    
    * darcy flow import update
    
    * refactor download fn
    
    * remove circ import from PDEDataset
    
    * remove partial small flow darcy for now
    
    * refactor to combine darcy and navier stokes b/c common format
    
    * darcy flow moved to base pde
    
    * web download base file
    
    * first try new darcy with zenodo
    
    * saving zenodo uploader
    
    * darcy tarfile api
    
    * darcy dataset with finished zenodo
    
    * test darcy download
    
    * export proper names from datasets module
    
    * comments + zenodo archive source
    
    * add load small darcy flow
    
    * export darcy flow small
    
    * cut download tests to only 16 res
    
    * add navier stokes form, pde-->ptdataset
    
    * os.path --> pathlib refs
    
    * load NS pt for backwards compat
    
    * add burgers in progress
    
    * fix subsampling rate per dim
    
    * add PT base class back in
    
    * add burgers class
    
    * add old burgers
    
    * add test web downloads
    
    * proper imports
    
    * update navier stokes for upload
    
    * remove burgers
    
    * update burgers train and test at 16 res
    
    * update tests, add burgers without download
    
    * update pt dataset docstring
    
    * add descriptions for burgers and darcy
    
    * add pt dataset test, 1d burgers dataset
    
    * remove old web download test
    
    * update default small darcy flow root + add 32 res darcy back
    
    * defineDeprecated fix
    
    * remove stale pt dataset
    
    * update burgers to str deprecation
    
    * fix NS dataset form for download
    
    * add __getitem__-correct swe dataset
    
    * add ns data test
    
    * add load_darcy_pt
    
    * remove stale comments in burgers
    
    * revert swe dataset to main
    
    * remove confirmation flags

--- neuralop/datasets/pt_dataset.py
+++
@@ -1,5 +0,0 @@
-from tensorly.utils import DefineDeprecated
-from neuralop.data.datasets import pt_dataset
-
-warning_msg = "Warning: neuralop.datasets.pt_dataset is deprecated and has been moved to neuralop.data.datasets.pt_dataset."
-load_pt_traintestsplit = DefineDeprecated(pt_dataset.load_pt_traintestsplit, warning_msg)
