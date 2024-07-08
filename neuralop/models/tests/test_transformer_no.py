a4e87847a58e1b78858aaec580ac9cf40461b772
dhpitt <david.h.pitt@gmail.com>
2024-07-08 16:39:01 -0700

    add first draft test w/irregular mesh

--- neuralop/models/tests/test_transformer_no.py
+++ neuralop/models/tests/test_transformer_no.py
@@ -4,12 +4,16 @@ import pytest
 import math
 
 from ..transformer_no import TransformerNO
+from neuralop.layers.embeddings import regular_grid_nd
 
 
 @pytest.mark.parametrize(
-    "input_shape", [(32, 2048, 1), (32, 64, 64, 3)]   # a 1D case and a 2D case
+    "input_shape", [(32, 2048, 1), (32, 64, 64, 3)],   # a 1D case and a 2D case,
 )
-def test_TransformerNO(input_shape):
+@pytest.mark.parametrize(
+    "regular_grid", [True, False],
+)
+def test_TransformerNO(input_shape, regular_grid):
     if torch.has_cuda:
         device = torch.device('cuda:0')
     else:
@@ -19,9 +23,10 @@ def test_TransformerNO(input_shape):
         n_dim = 1
     else:
         n_dim = 2
-    data_channles = input_shape[-1]
+    batch_size = input_shape[0]
+    data_channels = input_shape[-1]
     model = TransformerNO(n_dim=n_dim,
-                          in_channels=data_channles,
+                          in_channels=data_channels,
                           out_channels=1,    # single channel output
                           encoder_hidden_channels=32,
                           decoder_hidden_channels=32,
@@ -34,12 +39,19 @@ def test_TransformerNO(input_shape):
     model.to(device)
 
     in_data = torch.randn(input_shape).to(device)
-    in_data = in_data.view(in_data.shape[0], -1, in_data.shape[-1])   # flatten the spatial dimensions
-    pos = []
-    for i in range(n_dim):
-        pos.append(torch.linspace(0, 1, input_shape[i+1]))
-    pos = torch.meshgrid(*pos, indexing='ij')
-    pos = torch.cat([p.flatten().view(1, -1, 1) for p in pos], dim=-1).to(device)
+    in_data = in_data.view(batch_size, -1, in_data.shape[-1])   # flatten the spatial dimensions
+
+    if regular_grid:
+        # apply regular grid-based pos embedding
+        grid_shape = input_shape[1:-1]
+        grids =  regular_grid_nd(resolutions=grid_shape, grid_boundaries=[[0,1]] * len(grid_shape))
+        if len(grid_shape) == 1:
+            pos_reg = grids[0].unsqueeze(-1)
+        else:
+            pos_reg = torch.cat([x.unsqueeze(-1) for x in grids], dim=-1)
+        pos = pos_reg.view(1, -1, pos_reg.shape[-1]).to(device)
+    else:
+        pos = torch.randn(1, in_data.shape[1], n_dim).to(device)
 
     # Test forward pass
     out = model(in_data, pos)
