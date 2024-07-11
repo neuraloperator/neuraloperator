9203b62895ef5f15c46ff7c5f1f61a893229b65d
dhpitt <david.h.pitt@gmail.com>
2024-07-11 15:13:56 -0700

    remove old callback imports

--- neuralop/training/__init__.py
+++ neuralop/training/__init__.py
@@ -1,5 +1,3 @@
 from .trainer import Trainer
 from .torch_setup import setup
-from .callbacks import (Callback,
-        CheckpointCallback, IncrementalCallback)
 from .training_state import load_training_state, save_training_state
