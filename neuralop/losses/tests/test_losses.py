5c3a11fdae91a39681ee6994de48d2c76b487e51
dhpitt <david.h.pitt@gmail.com>
2024-07-11 20:10:37 -0700

    first h1 loss test

--- neuralop/losses/tests/test_losses.py
+++ neuralop/losses/tests/test_losses.py
@@ -1,7 +1,7 @@
 import math
 import torch
 
-from ..data_losses import LpLoss, MSELoss
+from ..data_losses import LpLoss, H1Loss, MSELoss
 
 def test_lploss():
     l2_2d_mean = LpLoss(d=2, p=2, reductions='mean')
@@ -31,6 +31,23 @@ def test_lploss():
 
     sum_abs_l2_err = l2_2d_sum.abs(zeros, ones)
     assert sum_abs_l2_err.item() - 40. * math.pi / 2  <= eps
+
+def test_h1loss():
+    h1 = LpLoss(d=2, p=2, reductions='mean')
+    x = torch.randn(10, 4, 4)
+
+    abs_0 = h1.abs(x,x)
+    assert abs_0.item() == 0.
+
+    zeros = torch.zeros_like(x)
+    ones = torch.ones_like(x)
+
+    # H1 w/out normalizing constant, 
+    # finite-difference derivatives of both sides are zero
+    # sum of items in each element in ones is 16
+    # norm is 4
+    mean_abs_h1 = h1.abs(zeros, ones, h=1.)
+    assert mean_abs_h1.item() == 4.
 
 def test_mseloss():
     mse_2d = MSELoss(reductions='sum')
