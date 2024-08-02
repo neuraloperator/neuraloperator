da5b8b65599ddc994b18c0972c5eaf85d7337c5f
dhpitt <david.h.pitt@gmail.com>
2024-08-02 15:24:20 -0700

    change quote marks for consistency

--- neuralop/models/sfno.py
+++ neuralop/models/sfno.py
@@ -1,9 +1,9 @@
-'''
+"""
 SFNO - Spherical Fourier Neural Operator
 Replaces the default SpectralConv (a convolution in the frequency domain 
 over Fourier basis functions) with a SphericalConv (a convolution over the
 spherical harmonic basis functions)
-'''
+"""
 from ..layers.spherical_convolution import SphericalConv
 from .fno import FNO, partialclass
 
