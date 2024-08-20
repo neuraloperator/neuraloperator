7fc3e0c8427b60b2c3dfdfb64cc97cc4adfb79d2
dhpitt <david.h.pitt@gmail.com>
2024-08-20 15:29:51 -0700

    refactor into generic test of correct optim momentum

--- neuralop/training/tests/test_optim.py
+++ neuralop/training/tests/test_optim.py
@@ -1,36 +1,26 @@
 import torch
 from torch.nn import Parameter
 from torch.testing import assert_close
+import pytest
 
 from ..adamw import AdamW
 
-def test_complex_adamw_momentum():
+@pytest.mark.parametrize('adam_optimizer_cls', [AdamW])
+def test_correct_complex_adam_momentum(adam_optimizer_cls):
     param1 = Parameter((0. + 1.0j) * torch.ones((3,3), dtype=torch.cfloat))
     param2 = Parameter((0. + 1.0j) * torch.ones((3,3), dtype=torch.cfloat))
 
-    complex_optimizer = AdamW(params=[param1],
+    optimizer = adam_optimizer_cls(params=[param1],
                       betas=(0.5, 0.5))
-    orig_optimizer = torch.optim.AdamW(params=[param2],
-                                       betas=(0.5, 0.5),)
-    
 
     loss1 = torch.view_as_real((param1 * param1.conj())).sum()
     loss2 = torch.view_as_real((param2 * param2.conj())).sum()
     # grad x^2 = 2x, grads are all 0 + 2j
 
     loss1.backward()
-    complex_optimizer.step()
+    optimizer.step()
 
     # momentum value should be elemwise (2j * -2j * 0.5) = 2 + 0j
     # exp_avg_sq should be empty, meaning it is just momentum * (1-beta2)
-    assert_close(complex_optimizer.state[param1]["exp_avg_sq"], (2 + 0j) * torch.ones_like(param1))
+    assert_close(optimizer.state[param1]["exp_avg_sq"], (2 + 0j) * torch.ones_like(param1))
 
-    ###### Comparison: wrong optim from torch.optim.Adam
-    # values will be a 2-tensor stack [grad.real * zeros_like(grad), grad.imag * ones_like(grad)]
-    # grads are 0 + 2j so this will be [zeros, 2 * ones]
-    # squaring this, multiplying by 1-beta2=0.5, adding to 0s will be [zeros, 2 * ones]
-    # then casting back to complex should return [0 + 2j]
-    loss2.backward()
-    orig_optimizer.step()
-
-    assert_close(orig_optimizer.state[param2]["exp_avg_sq"], (0 + 2j) * torch.ones_like(param2))
