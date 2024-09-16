708925be68e7cdaee55aa2fc2040a9c31e417049
ashiq24 <ashiqbuet14@gmail.com>
2024-09-15 19:03:49 -0700

    codomain attention block added

--- neuralop/layers/tests/test_codano.py
+++ neuralop/layers/tests/test_codano.py
@@ -1,6 +1,6 @@
 import pytest
 import torch
-from ..codano import TnoBlock2d
+from ..codano import CODABlocks2D, CODABlocks3D
 from ..spectral_convolution import *
 
 
@@ -10,7 +10,8 @@ def test_Codano(token_codimension):
     """
     Test CoDA-NO layers 
     """
-    n_modes = [100, 100]
+    n_modes_2D = [100, 100]
+    n_modes_3D = [100, 100, 100]
     n_head=3
     token_codimension=2
     output_scaling_factor=None
@@ -29,8 +30,8 @@ def test_Codano(token_codimension):
     temperature=1.0
     kqv_non_linear=False
 
-    layer = TnoBlock2d(
-        n_modes=n_modes,
+    layer = CODABlocks2D(
+        n_modes=n_modes_2D,
         n_head=n_head,
         token_codimension=token_codimension,
         output_scaling_factor=output_scaling_factor,
@@ -51,6 +52,33 @@ def test_Codano(token_codimension):
     )
 
     x = torch.randn(2, 10, 128, 128)
+
+    out = layer(x)
+
+    assert out.shape == x.shape
+
+    layer = CODABlocks3D(
+        n_modes=n_modes_3D,
+        n_head=n_head,
+        token_codimension=token_codimension,
+        output_scaling_factor=output_scaling_factor,
+        incremental_n_modes=incremental_n_modes,
+        head_codimension=head_codimension,
+        SpectralConvolution=SpectralConvolution,
+        Normalizer=Normalizer,
+        joint_factorization=joint_factorization,
+        fixed_rank_modes=fixed_rank_modes,
+        implementation=implementation,
+        decomposition_kwargs=decomposition_kwargs,
+        fft_norm=fft_norm,
+        codimension_size=codimension_size,
+        per_channel_attention=per_channel_attention,
+        permutation_eq=permutation_eq,
+        temperature=temperature,
+        kqv_non_linear=kqv_non_linear,
+    )
+
+    x = torch.randn(2, 10, 128, 128, 128)
 
     out = layer(x)
 
