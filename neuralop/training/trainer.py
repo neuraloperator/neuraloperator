1e6271429cd06062ad1b2388f4b1f4e564a4a056
dhpitt <david.h.pitt@gmail.com>
2024-10-07 15:50:31 -0700

    load model to device properly in eval

--- neuralop/training/trainer.py
+++ neuralop/training/trainer.py
@@ -321,7 +321,7 @@ class Trainer:
         """
         # Ensure model and data processor are loaded to the proper device
 
-        if self.model.device != self.device:
+        if self.model.device != self.device or not hasattr(self.model, 'device'):
             self.model = self.model.to(self.device)
         if self.data_processor is not None and self.data_processor.device != self.device:
             self.data_processor = self.data_processor.to(self.device)
