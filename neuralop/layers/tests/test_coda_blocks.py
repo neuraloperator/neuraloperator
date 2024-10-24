c9f97a6aefeb769e760fb9b41a7e15c0bb63bc07
ashiq24 <ashiqbuet14@gmail.com>
2024-10-24 20:10:38 +0000

    documentation fix, test code fix, removig unused argumnet

--- neuralop/layers/tests/test_coda_blocks.py
+++ neuralop/layers/tests/test_coda_blocks.py
@@ -4,14 +4,13 @@ from ..coda_blocks import *
 from ..spectral_convolution import *
 
 
-@pytest.mark.parametrize('token_codimension',
-                         [1, 2, 5])
-def test_Codano(token_codimension):
+@pytest.mark.parametrize('token_codimension', [1, 2, 5])
+@pytest.mark.parametrize('n_dim', [2, 3])
+def test_Codano(token_codimension, n_dim):
     """
     Test CoDA-NO layers
     """
-    n_modes_2D = [64, 64]
-    n_modes_3D = [64, 64, 64]
+    n_modes = [64] * n_dim
     n_head = 3
     output_scaling_factor = None
     incremental_n_modes = None
@@ -30,7 +29,7 @@ def test_Codano(token_codimension):
     kqv_non_linear = False
 
     layer = CODABlocks(
-        n_modes=n_modes_2D,
+        n_modes=n_modes,
         n_head=n_head,
         token_codimension=token_codimension,
         output_scaling_factor=output_scaling_factor,
@@ -49,44 +48,17 @@ def test_Codano(token_codimension):
         temperature=temperature,
         kqv_non_linear=kqv_non_linear,
     )
-
-    x = torch.randn(2, 10, 128, 128)
+    
+    spatial_res = [128]*n_dim
+    x = torch.randn(2, 10, *spatial_res)
     out = layer(x)
     assert out.shape == x.shape
+
 
     # test different spatial resolution 
-    x = torch.randn(2, 10, 100, 100)
+    spatial_res = [100]*n_dim
+    x = torch.randn(2, 10, *spatial_res)
     out = layer(x)
     assert out.shape == x.shape
 
-    
 
-    layer = CODABlocks(
-        n_modes=n_modes_3D,
-        n_head=n_head,
-        token_codimension=token_codimension,
-        output_scaling_factor=output_scaling_factor,
-        incremental_n_modes=incremental_n_modes,
-        head_codimension=head_codimension,
-        spectral_convolution=spectral_convolution,
-        Normalizer=Normalizer,
-        joint_factorization=joint_factorization,
-        fixed_rank_modes=fixed_rank_modes,
-        implementation=implementation,
-        decomposition_kwargs=decomposition_kwargs,
-        fft_norm=fft_norm,
-        codimension_size=codimension_size,
-        per_channel_attention=per_channel_attention,
-        permutation_eq=permutation_eq,
-        temperature=temperature,
-        kqv_non_linear=kqv_non_linear,
-    )
-
-    x = torch.randn(2, 10, 128, 128, 128)
-    out = layer(x)
-    assert out.shape == x.shape
-
-    # test different spato-temporal resolution
-    x = torch.randn(2, 10, 100, 100, 100)
-    out = layer(x)
-    assert out.shape == x.shape
