d97c2f67adf81461eabdc3ef0477c45f8119f527
Jean Kossaifi <jean.kossaifi@gmail.com>
2024-11-03 10:34:16 +0100

    Update tensor_dataset.py: fix deprecated import

--- neuralop/datasets/tensor_dataset.py
+++ neuralop/datasets/tensor_dataset.py
@@ -1,4 +1,7 @@
 from tensorly.utils import DefineDeprecated
+from neuralop.data.datasets import tensor_dataset
+from warnings import warn
 
 warning_msg = "Warning: neuralop.datasets.tensor_dataset is deprecated and has been moved to neuralop.data.datasets.tensor_dataset."
-TensorDataset = DefineDeprecated('neuralop.data.datasets.tensor_dataset.TensorDataset', warning_msg)
+warn(warning_msg)
+TensorDataset = DefineDeprecated('neuralop.data.datasets.tensor_dataset.TensorDataset', tensor_dataset.TensorDataset)
