22cb8b79c8d4197cee85634763f9dbde5f058f80
dhpitt <david.h.pitt@gmail.com>
2024-11-13 07:09:56 -0800

    fix import

--- neuralop/data/datasets/__init__.py
+++ neuralop/data/datasets/__init__.py
@@ -1,7 +1,7 @@
 from .darcy import DarcyDataset, load_darcy_flow_small
 from .navier_stokes import NavierStokesDataset, load_navier_stokes_pt 
 from .pt_dataset import PTDataset
-from .burgers import load_burgers_1dtime
+from .burgers import Burgers1dTimeDataset
 from .dict_dataset import DictDataset
 from .mesh_datamodule import MeshDataModule
 from .car_cfd_dataset import CarCFDDataset
