79ec17beda04d1e6cc9cc1941c5f78e516a37eb9
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2024-12-17 18:17:46 -0800

    finished porting over initial udno, no more warnings/type errs

--- neuralop/models/udno.py
+++ neuralop/models/udno.py
@@ -16,18 +16,19 @@ from torch.nn import functional as F
 class UDNO(BaseModel, name="UDNO"):
     def __init__(
         self,
-        in_shape: Tuple[int, int],
         in_channels: int,
         out_channels: int,
+        in_shape: Tuple[int, int],
+        radius_cutoff: float,
         hidden_channels: int = 32,
         num_pool_layers: int = 4,
-        disco_kernel_shape: Tuple[int, int] = (3, 4),
-        radius_cutoff: Optional[float] = None,
+        disco_kernel_shape: int | List[int] = [3, 4],
+        disco_kernel_bias: bool = False,
         drop_prob: float = 0.0,
     ):
         super().__init__()
 
-        assert len(in_shape) == 2, "Input shape must be 2D"
+        assert len(in_shape) == 2, "Input shape must be a 2d tuple like Ex: (64, 64)"
 
         self.in_shape = in_shape
         self.in_channels = in_channels
@@ -35,14 +36,148 @@ class UDNO(BaseModel, name="UDNO"):
         self.hidden_channels = hidden_channels
         self.num_pool_layers = num_pool_layers
         self.disco_kernel_shape = disco_kernel_shape
+        self.disco_kernel_bias = disco_kernel_bias
         self.radius_cutoff = radius_cutoff
         self.drop_prob = drop_prob
+
+        self.down_sample_layers = nn.ModuleList(
+            [
+                DISCOBlock(
+                    in_channels,
+                    hidden_channels,
+                    in_shape,
+                    radius_cutoff,
+                    drop_prob,
+                    disco_kernel_shape,
+                    disco_kernel_bias,
+                )
+            ]
+        )
+        ch = hidden_channels
+        shape = (in_shape[0] // 2, in_shape[1] // 2)
+        radius_cutoff = radius_cutoff * 2
+        for _ in range(num_pool_layers - 1):
+            self.down_sample_layers.append(
+                DISCOBlock(
+                    ch,
+                    ch * 2,
+                    in_shape=shape,
+                    radius_cutoff=radius_cutoff,
+                    drop_prob=drop_prob,
+                    kernel_shape=self.disco_kernel_shape,
+                    kernel_bias=self.disco_kernel_bias,
+                )
+            )
+            ch *= 2
+            shape = (shape[0] // 2, shape[1] // 2)
+            radius_cutoff *= 2
+
+            # test commit
+
+        self.bottleneck = DISCOBlock(
+            ch,
+            ch * 2,
+            in_shape=shape,
+            radius_cutoff=radius_cutoff,
+            drop_prob=drop_prob,
+            kernel_shape=self.disco_kernel_shape,
+            kernel_bias=self.disco_kernel_bias,
+        )
+
+        self.up = nn.ModuleList()
+        self.up_transpose = nn.ModuleList()
+        for _ in range(num_pool_layers - 1):
+            self.up_transpose.append(
+                TransposeDISCOBlock(
+                    ch * 2,
+                    ch,
+                    in_shape=shape,
+                    radius_cutoff=self.disco_radius_cutoff,
+                    kernel_shape=self.disco_kernel_shape,
+                    kernel_bias=self.disco_kernel_bias,
+                )
+            )
+            shape = (shape[0] * 2, shape[1] * 2)
+            radius_cutoff /= 2
+            self.up.append(
+                DISCOBlock(
+                    ch * 2,
+                    ch,
+                    in_shape=shape,
+                    radius_cutoff=radius_cutoff,
+                    drop_prob=drop_prob,
+                    kernel_shape=self.disco_kernel_shape,
+                    kernel_bias=self.disco_kernel_bias,
+                )
+            )
+            ch //= 2
+
+        self.up_transpose.append(
+            TransposeDISCOBlock(
+                ch * 2,
+                ch,
+                in_shape=shape,
+                radius_cutoff=radius_cutoff,
+                kernel_shape=self.disco_kernel_shape,
+                kernel_bias=self.disco_kernel_bias,
+            )
+        )
+        shape = (shape[0] * 2, shape[1] * 2)
+        radius_cutoff /= 2
+        self.up.append(
+            nn.Sequential(
+                DISCOBlock(
+                    ch * 2,
+                    ch,
+                    in_shape=shape,
+                    radius_cutoff=radius_cutoff,
+                    drop_prob=drop_prob,
+                    kernel_shape=self.disco_kernel_shape,
+                    kernel_bias=self.disco_kernel_bias,
+                ),
+                nn.Conv2d(
+                    ch, self.out_chans, kernel_size=1, stride=1
+                ),  # 1x1 conv is always res-invariant (pixel wise channel transformation)
+            )
+        )
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        stack = []
+        output = image
+
+        # encoder: apply down-sampling layers
+        for layer in self.down_sample_layers:
+            output = layer(output)
+            stack.append(output)
+            output = F.avg_pool2d(output, kernel_size=2, stride=2, padding=0)
+
+        # bottleneck
+        output = self.bottleneck(output)
+
+        # decoder: apply up-sampling layers
+        for transpose, disco in zip(self.up_transpose, self.up):
+            downsample_layer = stack.pop()
+            output = transpose(output)
+
+            # reflect pad on the right/botton if needed to handle odd input dimensions
+            padding = [0, 0, 0, 0]
+            if output.shape[-1] != downsample_layer.shape[-1]:
+                padding[1] = 1  # padding right
+            if output.shape[-2] != downsample_layer.shape[-2]:
+                padding[3] = 1  # padding bottom
+            if torch.sum(torch.tensor(padding)) != 0:
+                output = F.pad(output, padding, "reflect")
+
+            output = torch.cat([output, downsample_layer], dim=1)
+            output = disco(output)
+
+        return output
 
 
 class DISCOBlock(nn.Module):
     """
-    A DISCO Block that consists of two DISCO layers each followed by
-    instance normalization, LeakyReLU activation and dropout.
+    A DISCO Encoder Block that consists of two Equidistant DISCO2d layers each
+    followed by instance normalization, LeakyReLU activation and dropout.
     """
 
     def __init__(
@@ -100,66 +235,36 @@ class TransposeDISCOBlock(nn.Module):
 
     def __init__(
         self,
-        in_chans: int,
-        out_chans: int,
-        radius_cutoff: float,
+        in_channels: int,
+        out_channels: int,
         in_shape: Tuple[int, int],
-        kernel_shape: Tuple[int, int] = (3, 4),
+        radius_cutoff: float,
+        kernel_shape: int | List[int] = [6, 7],
+        kernel_bias: bool = False,
     ):
-        """
-        Parameters
-        ----------
-        in_chans : int
-            Number of channels in the input.
-        out_chans : int
-            Number of channels in the output.
-        radius_cutoff : float
-            Control the effective radius of the DISCO kernel. Values are
-            between 0.0 and 1.0. The radius_cutoff is represented as a proportion
-            of the normalized input space, to ensure that kernels are resolution
-            invaraint.
-        in_shape : Tuple[int]
-            Unbatched spatial 2D shape of the input to this block.
-            Rrequired to dynamically compile DISCO kernels for resolution invariance.
-        kernel_shape : Tuple[int, int], optional
-            Shape of the DISCO kernel. Default is (3, 4). This corresponds to 3
-            rings and 4 anisotropic basis functions. Under the hood, each DISCO
-            kernel has (3 - 1) * 4 + 1 = 9 parameters, equivalent to a standard
-            3x3 convolution kernel.
-
-            Note: This is NOT kernel_size, as under the DISCO framework,
-            kernels are dynamically compiled to support resolution invariance
-        """
         super().__init__()
 
-        self.in_chans = in_chans
-        self.out_chans = out_chans
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.in_shape = in_shape
+        self.radius_cutoff = radius_cutoff
+        self.kernel_shape = kernel_shape
+        self.kernel_bias = kernel_bias
 
         self.layers = nn.Sequential(
             nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
-            DISCO2d(
-                in_chans,
-                out_chans,
-                kernel_shape=kernel_shape,
-                in_shape=(2 * in_shape[0], 2 * in_shape[1]),
-                bias=False,
-                radius_cutoff=(radius_cutoff / 2),
-                padding_mode="constant",
+            EquidistantDiscreteContinuousConv2d(
+                self.in_channels,
+                self.out_channels,
+                in_shape=(2 * self.in_shape[0], 2 * self.in_shape[1]),
+                out_shape=self.in_shape,  # TODO: fix
+                kernel_shape=self.kernel_shape,
+                bias=self.bias,
+                radius_cutoff=(self.radius_cutoff / 2),
             ),
-            nn.InstanceNorm2d(out_chans),
+            nn.InstanceNorm2d(out_channels),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
         )
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
-        """
-        Parameters
-        ----------
-        image : torch.Tensor
-            Input 4D tensor of shape `(N, in_chans, H, W)`.
-
-        Returns
-        -------
-        torch.Tensor
-            Output tensor of shape `(N, out_chans, H*2, W*2)`.
-        """
         return self.layers(image)
