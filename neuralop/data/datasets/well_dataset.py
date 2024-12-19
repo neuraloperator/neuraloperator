412adac2addf590a31ffbce8c3cdffccf3fc86b0
dhpitt <david.h.pitt@gmail.com>
2024-12-18 21:45:03 -0800

    check in change

--- neuralop/data/datasets/well_dataset.py
+++ neuralop/data/datasets/well_dataset.py
@@ -6,36 +6,69 @@ import torch
 
 from .tensor_dataset import TensorDataset
 from ..transforms.data_processors import DefaultDataProcessor
-from ..transforms.normalizers import UnitGaussianNormalizer
+from ..transforms.normalizers import UnitGaussianNormalizer, DictUnitGaussianNormalizer
 
 from the_well.data import WellDataset
 from the_well.utils.download import well_download
 
 class TheWellDataset:
-    def __init__(root_dir: Path, 
+    """__init__ _summary_
+        Base Class for TheWell datasets
+        
+        Parameters
+        ----------
+        root_dir : Path
+            _description_
+        well_dataset_name : _type_
+            _description_
+        n_train : int
+            _description_
+        n_test : int
+            _description_
+        download : bool, optional
+            _description_, by default True
+        return_grid : bool, optional
+            _description_, by default True
+        """
+    def __init__(self,
+                 root_dir: Path, 
                  well_dataset_name,
-                 n_train: int,
-                 n_test: int, 
                  download: bool=True,
                  return_grid: bool=True,
 
                  ):
         
-        """ Base Class for TheWell datasets
-        """
-        base_path = root_dir / f"datasets/{well_dataset_name}"
+        base_path = root_dir / f"datasets/{well_dataset_name}/data/"
 
         if download:
             for split in ['train', 'test']:
-                data_path = base_path / f"data/{split}"
+                data_path = base_path / split
                 if not data_path.exists():
-                    well_download(base_path=root_dir,
+                    well_download(root_dir,
                                 dataset=well_dataset_name,
                                 split=split,
                                 )
+        self._train_db = WellDataset(path=str(base_path / "train"),
+                                        n_steps_input=1,
+                                        n_steps_output=1,
+                                        return_grid=return_grid,
+                                        use_normalization=False)
         
-        dataset = WellDataset(path=base_path,
-                              n_steps_input=1,
-                              n_steps_output=1,
-                              return_grid=return_grid,
-                              )
+        self._test_db = WellDataset(path=str(base_path / "train"),
+                                        n_steps_input=1,
+                                        n_steps_output=1,
+                                        return_grid=return_grid,
+                                        use_normalization=False)
+        
+        self.normalizer = 
+        
+        
+    @property
+    def train_db(self):
+        return self._train_db
+    
+    @property
+    def test_db(self):
+        return self._test_db
+    
+    
