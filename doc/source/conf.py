bc78878bbc5fa89ed4876a8a2b00e8832db5e043
dhpitt <david.h.pitt@gmail.com>
2025-01-27 10:20:16 -0800

    cleanup conf, remove stale code

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,8 +27,6 @@ author = 'Jean Kossaifi, David Pitt, Nikola Kovachki, Zongyi Li and Anima Anandk
 import neuralop
 release = neuralop.__version__
 
-
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -45,31 +43,14 @@ extensions = [
     'sphinx_gallery.gen_gallery',
 ]
 
-# -- sphinx_gallery configuration
-from sphinx_gallery.gen_gallery import ExplicitOrder
+# -- sphinx_gallery configuration ---------------------------------
 
 sg_gallery_dir = 'auto_examples'
 sg_examples_dir = '../../examples'
 
-def subsection_title_order(src_dir: str):
-    """
-    src_dir : str
-        string name of source path
-    """
-
-    sub_folders = [p for p in Path(src_dir).iterdir() if p.is_dir()]
-    final_order = []
-    for sub_folder in sub_folders:
-        if sub_folder.name == "release_highlights":
-            final_order.insert(0, sub_folder)
-        else:
-            final_order.append(sub_folder)
-    return ExplicitOrder(final_order)
-    
 sphinx_gallery_conf = {
      'examples_dirs': [sg_examples_dir],   # path to your example scripts
      'gallery_dirs': [sg_gallery_dir],  # path to where to save gallery generated output
-      #'subsection_order' : subsection_title_order(sg_examples_dir), # TODO@DAVID: turns out unneeded
      'nested_sections': True, # ensure we can nest multiple levels in the gallery toctree
 }
 
