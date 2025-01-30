3bcd4f061dec46d0689d7c13097cd07e3bbfb9fe
David Pitt <david.h.pitt@gmail.com>
2025-01-30 12:58:55 -0800

    Add tensor-GaLore to the main library (#510)
    * add tg projector and add documentation
    
    * add galore adamw
    
    * reformat old adamw docstring
    
    * add tests for tensorgalore
    
    * remove stale prints
    
    * remove stale print and update args
    
    * full citation for galore:
    
    * add full refs for tg projector
    
    * remove galoreadamw and merge tensorgalore into regular adamw

--- neuralop/training/adamw.py
+++ neuralop/training/adamw.py
@@ -1,32 +1,61 @@
-# copy dependencies from transformers/optimization.py
 import math
 import warnings
-from typing import Callable, Iterable, Tuple, Union, List
+from typing import Callable, Iterable, Tuple, Union
 
 import torch
 from torch import nn
 from torch.optim import Optimizer
+from .tensor_galore_projector import TensorGaLoreProjector
 
 
 class AdamW(Optimizer):
     """
-    Implements Adam algorithm with weight decay fix as introduced in [Decoupled Weight Decay
-    Regularization](https://arxiv.org/abs/1711.05101).
+    Implements AdamW (Adam with weight decay fix [1]_), and offers
+    optional Tensor-GaLore projection (see [2]_ and [3]_) for memory-efficient training.
 
     Parameters
     ----------
-    params : Iterable[nn.parameter.Parameter]
+    params: `Iterable[nn.parameter.Parameter]`:
         Iterable of parameters to optimize or dictionaries defining parameter groups.
-    lr : float, *optional*, defaults to 0.001
+    lr : `float`, *optional*, defaults to 0.001:
         The learning rate to use.
-    betas (`Tuple[float,float]`, *optional*, defaults to `(0.9, 0.999)`):
+    betas : `Tuple[float,float]`, *optional*, defaults to `(0.9, 0.999)`
         Adam's betas parameters (b1, b2).
-    eps (`float`, *optional*, defaults to 1e-06):
+    eps : `float`, *optional*, defaults to 1e-06:
         Adam's epsilon for numerical stability.
-    weight_decay (`float`, *optional*, defaults to 0.0):
+    weight_decay : `float`, *optional*, defaults to 0.0:
         Decoupled weight decay to apply.
-    correct_bias (`bool`, *optional*, defaults to `True`):
+    correct_bias : `bool`, *optional*, defaults to `True`:
         Whether or not to correct bias in Adam (for instance, in Bert TF repository they use `False`).
+    galore_params: `Iterable[nn.parameter.Parameter]`:
+        Iterable of parameters to optimize in low-rank subspace
+    galore_rank : float, int or int tuple
+        Rank of low-rank subspace in which to optimize `galore_params`.
+        Either a float corresponding to a percentage of params
+        to preserve, or an list of int ranks corresponding to
+        each mode of the tensor. If a single int is given, it is used 
+        for all modes (see `neuralop/training/tensor_galore_projector.py`)
+    galore_update_proj_gap : `int`, defaults to 50
+        Number of optimizer steps before projection tensors are recomputed,
+    galore_scale : `float`, defaults to 1.0
+        Additional lr-like scalar by which galore parameters are multiplied before update
+    activation_checkpoint: bool, default False
+        whether to use activation checkpointing during projection
+    no_deprecation_warning : `bool`, *optional*, defaults to `False`:
+        A flag used to disable the deprecation warning (set to `True` to disable the warning).
+
+    References
+    ----------
+    .. _[1] : Loschchilov, I. and Hutter, F. (2019). Decoupled Decay Regularization.
+         ICLR 2019, https://arxiv.org/pdf/1711.05101.
+
+    .. _[2] : Zhao, J, Zhang, Z., Chen, B., Wang, Z., Anandkumar, A., Tian Y. (2024). 
+        GaLore: Memory-Efficient LLM Training by Gradient Low-Rank Projection. ICML 2024,
+        https://arxiv.org/abs/2403.03507.
+    
+    .. _[3] : George, R., Pitt, D., Zhao, J., Kossaifi, J., Luo, C., Tian, Y., Anandkumar, A (2024). 
+        Tensor-GaLore: Memory-Efficient Training via Gradient Tensor Decomposition. arXiv preprint, 
+        https://arxiv.org/pdf/2501.02379.
     """
 
     def __init__(
@@ -37,7 +66,14 @@ class AdamW(Optimizer):
         eps: float = 1e-6,
         weight_decay: float = 0.0,
         correct_bias: bool = True,
-        ):
+        galore_params: Iterable[nn.parameter.Parameter]=None,
+        galore_rank: Union[float, int, Tuple[int]]=1.0, 
+        galore_update_proj_gap: int=50,
+        galore_scale: float=1.0,
+        activation_checkpoint: bool = False,
+        warm_restart: bool=True,
+    ):
+        
         if lr < 0.0:
             raise ValueError(f"Invalid learning rate: {lr} - should be >= 0.0")
         if not 0.0 <= betas[0] < 1.0:
@@ -48,7 +84,25 @@ class AdamW(Optimizer):
             raise ValueError(f"Invalid epsilon value: {eps} - should be >= 0.0")
         defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay, "correct_bias": correct_bias}
         super().__init__(params, defaults)
-    
+        
+        # Keep optional GaLore parameters separate for projection
+        if galore_params is not None:
+        
+            self.add_param_group({'params': galore_params,
+                                'rank': galore_rank,
+                                'lr': lr,
+                                'betas': betas,
+                                'eps': eps,
+                                'weight_decay': weight_decay,
+                                'correct_bias': correct_bias,
+                                'galore': True
+                                })
+        self.galore_rank = galore_rank
+        self.activation_checkpoint = activation_checkpoint
+        self.warm_restart = warm_restart
+        self.galore_update_proj_gap = galore_update_proj_gap
+        self.galore_scale = galore_scale
+
     @torch.no_grad()
     def step(self, closure: Callable = None):
         """
@@ -73,10 +127,21 @@ class AdamW(Optimizer):
                 
                 if "step" not in state:
                     state["step"] = 0
-                    
-                if 'dim' not in group:
-                    group['dim'] = 2
                 
+                # GaLore Projection
+                if group.get('galore', False):
+                    if "projector" not in state:
+                        state["projector"] = TensorGaLoreProjector(
+                            rank=self.galore_rank, 
+                            update_proj_gap=self.galore_update_proj_gap,
+                            scale=self.galore_scale, 
+                            activation_checkpoint=self.activation_checkpoint,
+                            warm_restart=self.warm_restart)
+                    
+                    # track tensor shape for projection back
+                    proj_input = grad
+                    grad = state["projector"].project(proj_input, state["step"])
+
                 # State initialization
                 if "exp_avg" not in state:
                     # Exponential moving average of gradient values
@@ -92,7 +157,6 @@ class AdamW(Optimizer):
                 # Decay the first and second moment running average coefficient
                 # In-place operations to update the averages at the same time
                 exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
-                
                 if torch.is_complex(grad):
                     exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1.0 - beta2)
                 else:
@@ -107,7 +171,11 @@ class AdamW(Optimizer):
 
                 # compute norm gradient
                 norm_grad = exp_avg / denom
-
+                
+                # GaLore Projection Back
+                if group.get('galore', False):
+                    norm_grad = state["projector"].project_back(norm_grad)
+                    
                 p.add_(norm_grad, alpha=-step_size)
 
                 # Just adding the square of the weights to the loss function is *not*
--- neuralop/training/tests/test_optim.py
+++ neuralop/training/tests/test_optim.py
@@ -2,8 +2,13 @@ import torch
 from torch.nn import Parameter
 from torch.testing import assert_close
 import pytest
+import math
 
 from ..adamw import AdamW
+
+from ..tensor_galore_projector import TensorGaLoreProjector
+
+from tensorly.tucker_tensor import validate_tucker_rank
 
 @pytest.mark.parametrize('adam_optimizer_cls', [AdamW])
 def test_correct_complex_adam_momentum(adam_optimizer_cls):
@@ -24,3 +29,41 @@ def test_correct_complex_adam_momentum(adam_optimizer_cls):
     # exp_avg_sq should be empty, meaning it is just momentum * (1-beta2)
     momentum = optimizer.state[param]["exp_avg_sq"]
     assert_close(momentum, (2 * x**2).to(torch.cfloat))
+
+
+@pytest.mark.parametrize('galore_param_pct', [0.25, 0.5, 1.0])
+@pytest.mark.parametrize('n_dim', [2,3,4])
+def test_galore_projector(galore_param_pct, n_dim):
+    full_rank_tensor = torch.randn([8]*n_dim)
+
+    galore_rank = validate_tucker_rank(full_rank_tensor.shape, galore_param_pct)
+    projector = TensorGaLoreProjector(rank=galore_rank, warm_restart=True)
+
+    # test that the projection is the correct size
+    # test 0th iter for full computation
+    low_rank_tensor = projector.project(full_rank_tensor, 0)
+    assert low_rank_tensor.numel() == math.prod(galore_rank)
+
+@pytest.mark.parametrize('galore_param_pct', [0.25, 0.5, 1.0])
+def test_galore_adamw_rank(galore_param_pct):
+    x = torch.randn((8,8,8,8), dtype=torch.float64)
+    param = Parameter(((0. + 1.0j) * x).to(torch.cfloat))
+    galore_param = Parameter(((1. + 0j) * x).to(torch.cfloat))
+
+    # pick mode-wise rank so that low_rank_grad.numel() / full_rank_grad.numel() == galore_param_pct
+    galore_rank = validate_tucker_rank(galore_param.shape, galore_param_pct)
+
+    optimizer = AdamW(params=[param],
+                            galore_params=[galore_param],
+                            galore_rank=galore_rank,
+                            betas=(0.5, 0.5))
+
+    loss = torch.view_as_real((param * galore_param.conj())).sum()
+
+    loss.backward()
+    optimizer.step()
+
+    momentum = optimizer.state[galore_param]["exp_avg_sq"]
+    # make sure low-rank params are being stored
+    assert momentum.numel() == math.prod(galore_rank)
+
