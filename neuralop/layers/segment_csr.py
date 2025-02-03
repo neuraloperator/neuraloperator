aa7133f26d97024cf9f68195f02c0ae3d6d15ecd
dhpitt <david.h.pitt@gmail.com>
2025-02-03 11:26:55 -0800

    segment csr with broadcasting, first draft

--- neuralop/layers/segment_csr.py
+++ neuralop/layers/segment_csr.py
@@ -59,24 +59,60 @@ def segment_csr(
                   Defaulting to naive PyTorch implementation")
         # if batched, shape [b, n_reps, channels]
         # otherwise shape [n_reps, channels]
+        """segment_csr reduces all entries of a CSR-formatted
+    matrix by summing or averaging over neighbors.
+
+    Used to reduce features over neighborhoods
+    in neuralop.layers.IntegralTransform
+
+    If use_scatter is set to False or torch_scatter is not
+    properly built, segment_csr falls back to a naive PyTorch implementation
+
+    Note: the native version is mainly intended for running tests on 
+    CPU-only GitHub CI runners to get around a versioning issue. 
+    torch_scatter should be installed and built if possible. 
+
+    Parameters
+    ----------
+    src : torch.Tensor
+        tensor of features for each point
+    indptr : torch.Tensor
+        splits representing start and end indices
+        of each neighborhood in src
+    reduce : Literal['mean', 'sum']
+        how to reduce a neighborhood. if mean,
+        reduce by taking the average of all neighbors.
+        Otherwise take the sum.
+    eps : float
+        Tiny perturbation to prevent div by zero in scaling
+        neigborhoods if a particular neighborhood is empty
+    use_scatter : bool
+        whether to use `torch_scatter`'s implementation of 
+        `segment_csr` over the native torch version. Defaults to True
+    """
+
         if src.ndim == 3:
             point_dim = 1
         else:
             point_dim = 0
 
         # if batched, shape [b, n_out, channels]
-        # otherwise shape [n_out, channels]
-        output_shape = list(src.shape)
-        n_out = indptr.shape[0] - 1
+        # otherwise shape [n_out, channels]    
+        n_in = src.shape[point_dim]
+
         n_reps = indptr[1:] - indptr[:-1]
 
-        output_shape[point_dim] = n_out
+        inds = torch.arange(n_in).unsqueeze(0)
+        mask = (inds >= indptr[:-1].unsqueeze(1)) & (inds < indptr[1:].unsqueeze(1))
 
-        indices = torch.arange(n_out).to(indptr.device).repeat_interleave(n_reps)
-        out = torch.zeros(output_shape, device=src.device).index_add_(point_dim, indices, src)
+        # add a batch dim to the mask if src is batched
+        if src.ndim == 3:
+            mask = mask.unsqueeze(0)
+        out = (mask.unsqueeze(-1).to(src.dtype) * src.unsqueeze(point_dim)).sum(dim=point_dim+1)
+
         if reduce == 'mean':
             # scale the outputs by number of reduced neighbors, add eps to avoid div by zero
             scale = (1 / (n_reps + eps)).unsqueeze(0).T
-            out = out * scale    
+            out = out * scale
 
         return out
