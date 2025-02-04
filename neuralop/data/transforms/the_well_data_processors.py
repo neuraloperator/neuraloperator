a7e30c8094b5c3c77739d81500948ab78f61eedd
dhpitt <david.h.pitt@gmail.com>
2025-02-04 13:40:52 -0800

    add thewelldataset in next step mode

---
+++ neuralop/data/transforms/the_well_data_processors.py
@@ -0,0 +1,51 @@
+from ..transforms.data_processors import DefaultDataProcessor
+from einops import rearrange
+
+import torch
+
+class TheWellDataProcessor(DefaultDataProcessor):
+    def __init__(self, normalizer):
+        super().__init__()
+        self.normalizer = normalizer
+        
+    def preprocess(self, data_dict, step=0):
+        """
+        Code adapted from the_well.data.data_formatter.DefaultChannelsFirstFormatter
+        """
+        x = data_dict["input_fields"]
+        x = rearrange(x, "b t ... c -> b (t c) ...")
+        if "constant_fields" in data_dict:
+            flat_constants = rearrange(data_dict["constant_fields"], "b ... c -> b c ...")
+            x = torch.cat(
+                [
+                    x,
+                    flat_constants,
+                ],
+                dim=1,
+            )
+        y = data_dict["output_fields"]
+
+        # TODO - Add warning to output if nan has to be replaced
+        # in some cases (staircase), its ok. In others, it's not.
+        if self.normalizer is not None:
+            x = self.normalizer.transform(x)
+            y = self.normalizer.transform(y)
+
+        data_dict["x"] = x
+        data_dict["y"] = y
+
+        return data_dict
+
+    def preprocess(self, output, data_dict, step=0):
+        """
+        Code adapted from the_well.data.data_formatter.DefaultChannelsFirstFormatter
+        """
+
+        #TODO@DAVID: handle step in autoregressive mode
+        y = data_dict["y"]
+        if self.normalizer is not None and not self.training:
+            y = self.normalizer.inverse_transform(y)
+            output = self.normalizer.inverse_transform(output)
+        
+        data_dict["y"] = y
+        return output, data_dict
