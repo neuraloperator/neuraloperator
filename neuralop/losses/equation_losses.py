5e73afbe4628cf99f395213f8f175eecf21cc595
dhpitt <david.h.pitt@gmail.com>
2025-02-18 07:08:28 -0800

    add poisson eqn loss

--- neuralop/losses/equation_losses.py
+++ neuralop/losses/equation_losses.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 
+from torch.autograd import grad
+
 from .data_losses import central_diff_2d
 
 
@@ -68,3 +70,111 @@ class ICLoss(object):
 
     def __call__(self, y_pred, x, **kwargs):
         return self.initial_condition_loss(y_pred, x)
+
+
+class PoissonEqnLoss(object):
+    def __init__(self, method='autograd', loss=F.mse_loss, 
+                 device='cuda', debug=False):
+        super().__init__()
+        self.method = method
+        self.loss = loss
+        self.debug = debug
+        self.device = device
+
+        
+    def finite_difference(self, output_queries_domain, u, output_source_terms_domain, num_boundary, out_sub_level, **kwargs):
+        # WARNING: used when domain is a structured grid
+        domain_length = 1/96
+
+        u_x, u_y = central_diff_2d(u.reshape((out_sub_level, out_sub_level)), [domain_length, domain_length], fix_x_bnd=True, fix_y_bnd=True)
+
+        u_xx, _ = central_diff_2d(u_x, [domain_length, domain_length], fix_x_bnd=True, fix_y_bnd=True)
+        _, u_yy = central_diff_2d(u_y, [domain_length, domain_length], fix_x_bnd=True, fix_y_bnd=True)
+
+        u_xx = u_xx.squeeze(0)
+        u_yy = u_yy.squeeze(0)
+        u = u.squeeze([0, -1])
+
+        # compute LHS of the Poisson equation
+        u_sq = torch.pow(u, 2)
+        u_x = u_x.reshape((out_sub_level**2))
+        u_y = u_y.reshape((out_sub_level**2))
+        u_xx = u_xx.reshape((out_sub_level**2))
+        u_yy = u_yy.reshape((out_sub_level**2))
+        laplacian = (u_xx + u_yy)
+
+        norm_grad_u = u_x ** 2 + u_y ** 2
+
+        left_hand_side = laplacian + laplacian * 0.1 * u_sq + 0.2 * u * norm_grad_u
+        output_source_terms_domain = output_source_terms_domain.squeeze(0).squeeze(-1)
+
+        assert left_hand_side.shape == output_source_terms_domain.shape
+        loss = self.loss(left_hand_side, output_source_terms_domain)
+        del u_xx, u_yy, u_x, u_y, left_hand_side
+        return loss
+    
+    def autograd(self, output_queries_domain, u, output_source_terms_domain, num_boundary, out_sub_level, **kwargs):
+        # compute the nonlinear Poisson equation: ∇·((1 + 0.1u^2)∇u(x)) = f(x)
+        u_prime = grad(outputs=u.sum(), inputs=output_queries_domain, create_graph=True, retain_graph=True)[0]
+        
+        norm_grad_u = torch.pow(u_prime, 2).sum(dim=-1)
+        # return None, norm_grad_u, None
+
+        assert output_queries_domain.shape[-1] == 2
+        # assert output_queries_domain.shape[0] == 1
+        assert output_queries_domain.ndim == 3
+
+        u_x = u_prime[:,:,0]
+        u_y = u_prime[:,:,1]
+        
+        # compute second derivatives
+        u_xx = grad(outputs=u_x.sum(), inputs=output_queries_domain, create_graph=True, retain_graph=True)[0][:,:,0]
+        u_yy = grad(outputs=u_y.sum(), inputs=output_queries_domain, create_graph=True, retain_graph=True)[0][:,:,1]
+
+        u_xx = u_xx.squeeze(0)
+        u_yy = u_yy.squeeze(0)
+        u_prime = u_prime.squeeze(0)
+        u = u.squeeze([0, -1])
+
+        # compute LHS of the Poisson equation
+        u_sq = torch.pow(u, 2)
+        laplacian = (u_xx + u_yy)
+        norm_grad_u = torch.pow(u_prime, 2).sum(dim=-1)
+
+        assert u_sq.shape == u_xx.shape == u_yy.shape == norm_grad_u.shape
+
+        left_hand_side = laplacian + laplacian * 0.1 * u_sq + 0.2 * u * norm_grad_u
+        output_source_terms_domain = output_source_terms_domain.squeeze(0)
+
+        assert left_hand_side.shape == output_source_terms_domain.shape
+        loss = self.loss(left_hand_side, output_source_terms_domain)
+        assert not u_prime.isnan().any()
+        assert not u_yy.isnan().any()
+        assert not u_xx.isnan().any()
+
+        return loss
+    
+    def __call__(self, y_pred, **kwargs):
+        if self.method == "autograd":
+            return self.autograd(u=y_pred, **kwargs)
+        elif self.method == "finite_difference":
+            return self.finite_difference(u=y_pred, **kwargs)
+        else:
+            raise NotImplementedError()
+    
+class PoissonBoundaryLoss(object):
+    def __init__(self, loss=F.mse_loss, 
+                 device='cuda', debug=False):
+        super().__init__()
+        self.loss = loss
+        self.debug = debug
+        self.device = device
+        self.counter = 0
+
+    def __call__(self, y_pred, num_boundary, out_sub_level, y, output_queries, **kwargs):
+        num_boundary = int(num_boundary.item() * out_sub_level)
+        boundary_pred = y_pred.squeeze(0).squeeze(-1)[:num_boundary]
+        y_bound = y.squeeze(0).squeeze(-1)[:num_boundary]
+        
+        assert boundary_pred.shape == y_bound.shape
+        return self.loss(boundary_pred, y_bound)
