9a71b19ba9b2e4a8d9115841b2f97e24f4b49eb4
dhpitt <david.h.pitt@gmail.com>
2025-03-10 16:05:24 -0700

    fix batching for weights (rep_features never has batch > 1)

--- neuralop/layers/integral_transform.py
+++ neuralop/layers/integral_transform.py
@@ -191,17 +191,14 @@ class IntegralTransform(nn.Module):
             rep_features.mul_(in_features)
 
         nbr_weights = neighbors.get("weights")
+        print()
         if nbr_weights is None and self.weighting_fn is not None:
             raise KeyError("if a weighting function is provided, your neighborhoods must contain weights.")
         
         if self.weighting_fn is not None:
             assert nbr_weights.ndim == 1
             nbr_weights = self.weighting_fn(nbr_weights).unsqueeze(-1).unsqueeze(0)
-            if batched:
-                # repeat weights along batch dim if batched                                                                                                                                                
-                nbr_weights = nbr_weights.repeat(                                                                                                                                  
-                    [batch_size] + [1] * nbr_weights.ndim                                                                                                                          
-                )
+            print(f"{nbr_weights.shape=}")
             rep_features.mul_(nbr_weights)                                                                                                                           
             
             
