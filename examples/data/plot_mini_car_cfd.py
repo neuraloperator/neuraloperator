d63c1143c63c1e05e2527a05466c2d258f6e7808
dhpitt <david.h.pitt@gmail.com>
2025-05-07 11:20:09 -0700

    draw arrows and clarify plots

--- examples/data/plot_mini_car_cfd.py
+++ examples/data/plot_mini_car_cfd.py
@@ -80,14 +80,19 @@ ax.legend()
 ax.view_init(elev=20, azim=150, roll=0, vertical_axis='y')
 ax.set_title("Query points and vertices")
 # %%
-# Visualizing neighbor search
+# Neighbor search between 3D point clouds
 # In :doc:`../layers/plot_neighbor_search` we demonstrate our neighbor search
 # on a simple 2-d point cloud. Let's try that again with our points here:
 
 from neuralop.layers.neighbor_search import native_neighbor_search
 verts = sample['vertices']
-query_point = queries[1000]
-nbr_data = native_neighbor_search(data=verts, queries=query_point.unsqueeze(0), radius=0.15)
+#query_point = queries[1000]
+query_point = queries[3300] # 1550 and 0.4 is really good
+#nbr_data = native_neighbor_search(data=verts, queries=query_point.unsqueeze(0), radius=0.15)
+nbr_data = native_neighbor_search(data=verts, queries=query_point.unsqueeze(0), radius=0.5)
+
+# %% Visualizing neighborhoods
+# Let's plot the new neighbors we just found on top of the car surface point cloud.
 fig = plt.figure(figsize=(8,10))
 ax1 = fig.add_subplot(2,1,1, projection='3d')
 ax2 = fig.add_subplot(2,1,2, projection='3d')
@@ -125,6 +130,65 @@ for ax in ax1,ax2:
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_zticks([])
+plt.draw()
+
+
+# %%
+# **Connecting neighbors to query**
+#
+# First, let's make a simple utiltiy to add arrows to our 3D plot:
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        super().__init__((0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def do_3d_projection(self, renderer=None):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+
+        return np.min(zs)
+
+# Creating plots
+fig = plt.figure(figsize=(8,10))
+ax1 = fig.add_subplot(projection='3d')
+neighbors = verts[nbr_data['neighbors_index']]
+
+# Plotting just one query point vs. the car
+ax1.scatter(verts[:, 0], verts[:, 1], verts[:, 2]*2, s=1, alpha=0.1)
+ax1.scatter(query_point[0], query_point[1], query_point[2]*2, s=10, c='red', label='Single query')
+ax1.scatter(neighbors[:, 0], neighbors[:, 1], neighbors[:, 2]*2, s=10, label="Neighbors on\ncar surface",)
+
+ax1.view_init(elev=20, azim=-20, roll=0, vertical_axis='y')
+ax1.legend()
+ax1.set_xlim(0,2)
+ax1.set_ylim(0,2)
+ax1.set_xlabel("x")
+ax1.set_ylabel("y")
+ax1.set_zlabel("z")
+ax1.view_init(elev=20, azim=-20, roll=0, vertical_axis='y')
+ax1.grid(False)
+ax1.set_title("One query point")
+
+
+for ax in [ax1]:
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+
+# Add arrows between neighbors and query
+arrow_prop_dict = dict(mutation_scale=1, arrowstyle='-|>', color='red', shrinkA=1, shrinkB=1, alpha=0.1)
+for nbr in neighbors:
+    a = Arrow3D([query_point[0], nbr[0]],
+                [query_point[1], nbr[1]], 
+                [query_point[2]*2, nbr[2]*2], **arrow_prop_dict)
+    ax1.add_artist(a)
 
 fig.tight_layout()
 plt.draw()
