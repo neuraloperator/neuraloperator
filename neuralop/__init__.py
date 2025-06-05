9de96dadb5c9ef9d893b4578ab1cd4727cd1febd
Valentin Duruisseaux <81578633+vduruiss@users.noreply.github.com>
2025-06-05 00:11:22 -0700

    Small Fixes

--- neuralop/__init__.py
+++ neuralop/__init__.py
@@ -4,5 +4,5 @@ from .models import TFNO3d, TFNO2d, TFNO1d, TFNO
 from .models import get_model
 from .data import datasets, transforms
 from . import mpu
-from .training import Trainer
-from .losses import LpLoss, H1Loss, BurgersEqnLoss, ICLoss, WeightedSumLoss
+from .training import Trainer, PINOTrainer
+from .losses import LpLoss, H1Loss, BurgersEqnLoss, ICLoss, WeightedSumLoss, FieldwiseAggregatorLoss, Relobralo, SoftAdapt
