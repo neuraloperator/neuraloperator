bb12c6c06c60e9dfe3e4c9bc30e3df2e7a7e42fa
Sina Pordanesh <sina.pordanesh@yahoo.com>
2025-09-09 17:18:30 -0600

    Add extra test cases - weight initialization, being complex and expected shape - make sure kernel ouput be in real space assertion - linspace testing (important for LNO): LNO relies on tailored linspace discritization domain for each operation, and user must frequently set domain discritiztion parameters, depending to the problem. Check discritization funcitonality on test cases.

--- neuralop/layers/tests/test_spectral_convolution_laplace.py
+++ neuralop/layers/tests/test_spectral_convolution_laplace.py
@@ -25,8 +25,14 @@ def test_SpectralConvLaplace(dim, in_channels, out_channels, output_shape):
         in_channels=in_channels,
         out_channels=out_channels,
         n_modes=modes[:dim],
-        bias=True,
+        bias=False,
     )
+    
+    # Basic parameter checks - Complex weights
+    assert torch.is_complex(conv.weight)
+    # Expected weight last dimension
+    expected_weight_last_dim = sum(modes[:dim]) + int(np.prod(modes[:dim]))
+    assert tuple(conv.weight.shape) == (in_channels, out_channels, expected_weight_last_dim)
     
     # Input tensor
     x = torch.randn(batch_size, in_channels, *(12,)*dim, dtype=dtype)
@@ -40,6 +46,8 @@ def test_SpectralConvLaplace(dim, in_channels, out_channels, output_shape):
         assert res.shape[0] == batch_size
         assert res.shape[1] == out_channels
         assert list(res.shape[2:]) == list(expected_output_shape[2:])
+        # Output should be real when input is real
+        assert not torch.is_complex(res)
     
     else:
         # Forward pass
@@ -49,3 +57,17 @@ def test_SpectralConvLaplace(dim, in_channels, out_channels, output_shape):
         assert res.shape[0] == batch_size
         assert res.shape[1] == out_channels
         assert list(res.shape[2:]) == [12]*dim
+        # Output should be real when input is real
+        assert not torch.is_complex(res)
+
+    # Check linspace with explicit parameters
+    from ..spectral_convolution_laplace import _compute_dt_nd
+    steps = tuple([10] * dim)
+    starts = [-1.0] * dim
+    ends = [2.0] * dim
+    dt_list, grid_coords_list = _compute_dt_nd(shape=steps, start_points=starts, end_points=ends)
+    for i in range(dim):
+        expected_grid = torch.linspace(starts[i], ends[i], steps=steps[i])
+        assert torch.allclose(grid_coords_list[i], expected_grid)
+        
+    
