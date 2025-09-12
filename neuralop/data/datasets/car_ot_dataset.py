877b7e4d1a599744816e5648a9a6cf4534e5a253
Xinyi Li <xyli4869@outlook.com>
2025-09-12 16:02:13 +0800

    create ot dataloader

---
+++ neuralop/data/datasets/car_ot_dataset.py
@@ -0,0 +1,90 @@
+from typing import List, Union
+from pathlib import Path
+
+import torch
+from torch.utils.data import DataLoader
+
+from .dict_dataset import DictDataset
+from ..transforms.normalizers import UnitGaussianNormalizer
+
+from .ot_datamodule import OTDataModule
+from .web_utils import download_from_zenodo_record
+from neuralop.utils import get_project_root
+
+class CarOTDataset(OTDataModule):
+    """
+    Parameters
+    ----------
+    root_dir : Union[str, Path]
+        root directory at which data is stored.
+    n_train : int, optional
+        Number of training instances to load, by default 1
+    n_test : int, optional
+        Number of testing instances to load, by default 1
+    query_res : List[int], optional
+        Dimension-wise resolution of signed distance function 
+        (SDF) query cube, by default [32,32,32]
+    download : bool, optional
+        Whether to download data from Zenodo, by default True
+    
+
+    Attributes
+    ----------
+    train_loader: torch.utils.data.DataLoader
+        dataloader of training examples
+    test_loader: torch.utils.data.DataLoader
+        dataloader of testing examples
+
+    """
+
+    def __init__(self,
+        root_dir: Union[str, Path],
+        n_train: int = 1,
+        n_test: int = 1,
+        expand_factor: float = 3.0, 
+        reg: float = 1e-06,
+        device: Union[str, torch.device] = 'cpu',
+        ):
+
+        if isinstance(root_dir, str):
+            root_dir = Path(root_dir)
+        
+        if not root_dir.exists():
+            root_dir.mkdir(parents=True)
+
+        super().__init__(
+            root_dir=root_dir,
+            item_dir_name='data',
+            n_total=n_train+n_test,
+            expand_factor=expand_factor, 
+            reg=reg,
+            device=device,
+        )
+        
+        # process data list to remove specific vertices from pressure to match number of vertices
+        for i, item_data in enumerate(self.data):
+            press = item_data['press']
+            self.data[i]['press'] = torch.cat((press[0:16], press[112:]), axis=0)
+        
+        # encode transport and pressure
+        normalizer_keys = ['trans','press']
+        self.normalizers = UnitGaussianNormalizer.from_dataset(
+                self.data[0:n_train], dim=[1], keys=normalizer_keys
+            )
+        
+        for attr in normalizer_keys:
+            for j in range(len(self.data)):
+                data_elem = self.data[j][attr]
+                self.data[j][attr] = self.normalizers[attr].transform(data_elem)
+
+        # Datasets
+        self.train_data = DictDataset(data[0:n_train])
+        self.test_data = DictDataset(data[n_train:])
+
+def load_saved_ot():
+    """
+    Load the 3-example mini Car-CFD dataset we package along with our module.
+
+    See `neuralop.data.datasets.CarCFDDataset` for more detailed references
+    """
+    return torch.load(get_project_root() / "neuralop/data/datasets/data/mini_car.pt")
---
+++ neuralop/data/datasets/ot_datamodule.py
@@ -0,0 +1,158 @@
+from pathlib import Path
+from timeit import default_timer
+from typing import List, Union
+
+import numpy as np
+
+# import open3d for io if built. Otherwise,
+# the class will build, but no files will be loaded.
+try:
+    import open3d as o3d
+    o3d_warn = False
+except ModuleNotFoundError:
+    o3d_warn = True
+
+try:
+    import ot
+    from ot.bregman import empirical_sinkhorn2_geomloss
+    ot_warn = False
+except ModuleNotFoundError:
+    ot_warn = True   
+
+import torch
+
+# Get the directory of the current script
+script_dir = Path(__file__).parent
+
+class OTDataModule:
+    def __init__(
+        self,
+        root_dir: Union[str, Path],
+        item_dir_name: Union[str, Path],
+        n_total: int = None,
+        expand_factor: float = 3.0, 
+        reg: float = 1e-06,
+        device: Union[str, torch.device] = 'cpu',
+    ):
+        """MeshDataModule provides a general dataset for irregular coordinate meshes
+            for use in a GNO-based architecture
+
+        Parameters
+        ----------
+        root_dir : Union[str, Path]
+            str or Path to root directory of CFD dataset
+        expand_factor:
+            expand the pysical size (car, target) to latent size (torus, source)
+        latent_res : List[int], optional
+            resolution of latent query points along each dimension
+        """
+        self.device = device
+
+        if o3d_warn or ot_warn:
+            print("Warning: you are attempting to run MeshDataModule without the required dependency open3d.")
+            raise ModuleNotFoundError()
+        if isinstance(root_dir, str):
+            root_dir = Path(root_dir)
+
+        # Ensure path is valid
+        root_dir = root_dir.expanduser()
+        assert root_dir.exists(), "Path does not exist"
+        assert root_dir.is_dir(), "Path is not a directory"
+
+        # Read all indicies
+        mesh_ind = self.read_indices(root_dir / 'watertight_meshes.txt')
+
+        if n_total is not None:
+            if n_total <= len(mesh_ind):
+                mesh_ind = mesh_ind[0:n_total]
+            else:
+                print(f"Warning: requested n_total ({n_total}) is greater than the number of available meshes ({len(mesh_ind)}). All available meshes will be used.")
+        n_total = len(mesh_ind)
+        # remove trailing newlines from train and test indices
+        mesh_ind = [x.rstrip() for x in mesh_ind]
+
+        data_dir = root_dir / item_dir_name
+
+        # Load all meshes and compute OT
+        data = []
+        n_non_surjective = 0
+        n_missed_points = 0
+        for ind in mesh_ind:
+            mesh = o3d.io.read_triangle_mesh(
+                str(data_dir / ("mesh_" + ind.zfill(3) + ".ply"))
+            )
+            target = torch.from_numpy(np.asarray(mesh.vertices).squeeze().astype(np.float32())).to(self.device) #(3586,3) car vertices
+            mesh.compute_vertex_normals()
+            normal = torch.from_numpy(np.asarray(mesh.vertex_normals).astype(np.float32()))
+            pressure = np.load(
+                str(data_dir / ('press_' + ind.zfill(3) + '.npy'))
+            ) # (3682,)
+            #pressure = np.concatenate((pressure[0:16], pressure[112:]), axis=0) #(3586,) this step moved to car_ot_dataset
+            pressure = torch.from_numpy(pressure.astype(np.float32()))
+
+            # OT
+            n_t = len(target) # number of target samples
+            n_s_sqrt = int(np.sqrt(expand_factor)*np.ceil(np.sqrt(n_t))) # sqrt of the number of source samples
+            source = self.torus_grid(n_s_sqrt) # build source gird
+            #_, log = empirical_sinkhorn2_geomloss(X_s=source.to(self.device), X_t=target, reg=reg, log=True) # utilize weighted Sinkhorn a=a.to(device), b=b.to(device),
+            #gamma = log['lazy_plan'][:].detach() # convert the lazy tensor to torch.tensor (dense)
+            n_s = n_s_sqrt**2
+            a, b = np.ones((n_s,)) / n_s, np.ones((n_t,)) / n_t
+            M = ot.dist(source.numpy(), target.numpy())
+            gamma = ot.emd(a, b, M, numItermax=1000000)
+            gamma = torch.from_numpy(gamma.astype(np.float32()))
+
+            # normalize the OT plan matrix by column
+            row_norms = torch.norm(gamma, p=1, dim=1, keepdim=True)
+            gamma_encoder = gamma / row_norms
+
+            # transport target to source
+            transport = torch.mm(gamma_encoder, target)
+
+            # encoder: target -> source
+            distances = torch.cdist(transport, target)
+            indices_encoder = torch.argmin(distances, dim=1) # find the closest point in "target" (car vertices) to each point in "transport" (latent grids)
+            
+            # reset the transport as the closest point in target
+            transport = target[indices_encoder]
+            unique = len(torch.unique(indices_encoder))
+            n_missed_points += n_t - unique
+            if unique!=n_t:
+                n_non_surjective += 1
+
+            # decoder: source -> target
+            indices_decoder = torch.argmin(distances, dim=0) #find the closest point in "transport" (latent grids) to each point in "target" (car vertices)
+
+            item_dict = {
+                "verts": target.cpu(),
+                "ind_enc": indices_encoder.cpu(),
+                "ind_dec": indices_decoder.cpu(),
+                "press": pressure,
+                "nor": normal,
+                "trans": transport.cpu()
+            }
+            data.append(item_dict)
+        self.data = data
+        torch.save(data, script_dir / "data" / f"ot_expand{expand_factor}_reg{reg}_num{n_total}.pt")
+        print(f"Number of none surjective OT plan is {n_non_surjective:d}; average missed points is {n_missed_points/n_total:.2f}")
+
+    def read_indices(self, file_path):
+        with open(file_path, 'r') as file:
+            indices = [line.strip() for line in file if line.strip().isdigit()]
+        return indices
+    
+    def torus_grid(self,n_s_sqrt):
+        theta = torch.linspace(0, 2 * torch.pi, n_s_sqrt + 1)[:-1]
+        phi = torch.linspace(0, 2 * torch.pi, n_s_sqrt + 1)[:-1]
+        # Create a grid using meshgrid
+        X, Y = torch.meshgrid(theta, phi, indexing='ij')
+        points = torch.stack((X, Y)).reshape((2, -1)).T
+
+        r = 0.5
+        R = 1.0
+        x = (R + r * torch.cos(points[:, 0])) * torch.cos(points[:, 1])
+        y = (R + r * torch.cos(points[:, 0])) * torch.sin(points[:, 1])
+        z = r * torch.sin(points[:, 0])
+        grid = torch.stack((x, y, z), axis=1)
+        
+        return grid
