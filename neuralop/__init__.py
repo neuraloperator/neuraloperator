1e2f252beee9755a05a04dbd8dd6693138ac74d9
Valentin Duruisseaux <81578633+vduruiss@users.noreply.github.com>
2025-10-18 21:15:36 -0700

    Small formatting updates

--- neuralop/__init__.py
+++ neuralop/__init__.py
@@ -4,4 +4,16 @@ from .models import TFNO, FNO, get_model
 from .data import datasets, transforms
 from . import mpu
 from .training import Trainer
-from .losses import LpLoss, H1Loss, BurgersEqnLoss, ICLoss, WeightedSumLoss, Aggregator, Relobralo, SoftAdapt, FourierDiff, non_uniform_fd, FiniteDiff
+from .losses import (
+    LpLoss,
+    H1Loss,
+    BurgersEqnLoss,
+    ICLoss,
+    WeightedSumLoss,
+    Aggregator,
+    Relobralo,
+    SoftAdapt,
+    FourierDiff,
+    non_uniform_fd,
+    FiniteDiff,
+)
